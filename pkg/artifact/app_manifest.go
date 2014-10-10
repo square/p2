3bdc289bfb5e01ddc79b73c4d8495d3fefb5c98d
abishopric <abishopric@squareup.com>
2014-10-09 18:58:01 -0700

    begin implementing app manifest package

---
+++ pkg/artifact/app_manifest.go
@@ -0,0 +1,31 @@
+package artifact
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+type AppManifest struct {
+	Ports map[int][]string `yaml:"ports"`
+}
+
+func ManifestFromPath(path string) (*AppManifest, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return ManifestFromBytes(bytes)
+}
+
+func ManifestFromBytes(bytes []byte) (*AppManifest, error) {
+	manifest := &AppManifest{}
+	if err := yaml.Unmarshal(bytes, manifest); err != nil {
+		return nil, err
+	}
+	return manifest, nil
+}
---
+++ pkg/artifact/app_manifest_test.go
@@ -0,0 +1,10 @@
+package artifact
+
+import (
+	. "github.com/anthonybishopric/gotcha"
+	"testing"
+)
+
+func TestAppManifestHasPorts(t *testing.T) {
+	Assert(t).IsTrue(true, "")
+}
