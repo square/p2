7c6a8210e78ebb85a72fd2bc3eecf99ab2c69fb8
abishopric <abishopric@squareup.com>
2014-10-16 13:01:14 -0700

    add error case test

--- pkg/runit/runit_service_test.go
+++ pkg/runit/runit_service_test.go
@@ -15,3 +15,10 @@ func TestRunitServicesCanBeStarted(t *testing.T) {
 	Assert(t).IsNil(err, "There should not have been an error starting the service")
 	Assert(t).AreEqual(out, "start /var/service/foo\n", "Did not start service with correct arguments")
 }
+
+func TestErrorReturnedIfRunitServiceBails(t *testing.T) {
+	sv := SV{util.From(runtime.Caller(0)).ExpandPath("erring_sv")}
+	service := &Service{"/var/service/foo", "foo"}
+	_, err := sv.Start(service)
+	Assert(t).IsNotNil(err, "There should have been an error starting the service")
+}
