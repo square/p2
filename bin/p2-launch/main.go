1a1fea90e12ccd513275c40dc0b2c01b0c6163df
tummychow <sjung@squareup.com>
2015-01-15 14:21:16 -0800

    Allow p2-launch to specify user
    The user that a pod executes under can now be configured, so the consul
    pod is not hardcoded to run as root. Unfortunately, this means you can't
    p2-launch the consul pod - it tries to run consul under the "intent"
    user.
    
    You have to be root to run p2-launch anyway, so we can add a flag to
    specify the user of the pod being launched to fix this issue.

--- bin/p2-launch/main.go
+++ bin/p2-launch/main.go
@@ -12,6 +12,7 @@ import (
 
 var (
 	manifestURI = kingpin.Arg("manifest", "a path or url to a pod manifest that will be installed and launched immediately.").String()
+	runUser     = kingpin.Flag("runas", "the user that the pod will execute as. Defaults to the pod's ID.").String()
 )
 
 func main() {
@@ -33,6 +34,9 @@ func main() {
 	}
 
 	pod := pods.NewPod(manifest.ID(), pods.PodPath(manifest.ID()))
+	if runUser != nil && *runUser != "" {
+		pod.RunAs = *runUser
+	}
 	err = pod.Install(manifest)
 	if err != nil {
 		log.Fatalf("Could not install manifest %s: %s", manifest.ID(), err)
