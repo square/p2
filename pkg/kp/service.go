f9d664d0320c0798ee751dea1bbe039e5a1fbb52
tummychow <sjung@squareup.com>
2015-01-16 16:16:20 -0800

    Add RegisterService for pkg/kp
    This function is an extra part of the pkg/intent interface that we want
    to replace.

---
+++ pkg/kp/service.go
@@ -0,0 +1,29 @@
+package kp
+
+import (
+	"fmt"
+
+	"github.com/armon/consul-api"
+	"github.com/square/p2/pkg/pods"
+)
+
+// RegisterService creates a consul service for the given pod manifest. If the
+// manifest specifies a status port, the resulting consul service will also
+// include a health check for that port.
+func (s *Store) RegisterService(manifest pods.PodManifest) error {
+	podService := &consulapi.AgentServiceRegistration{
+		Name: manifest.ID(),
+	}
+
+	if manifest.StatusPort != 0 {
+		podService.Port = manifest.StatusPort
+		podService.Check = &consulapi.AgentServiceCheck{
+			// prints the HTTP response on stderr, while exiting 0 if the status code is 200
+			Script: fmt.Sprintf(`if [[ $(curl 0.0.0.0:%v/_status -s -o /dev/stderr -w "%%{http_code}") == "200" ]] ; then exit 0 ; else exit 2; fi`, manifest.StatusPort),
+			// magic number alert
+			Interval: "5s",
+		}
+	}
+
+	return s.client.Agent().ServiceRegister(podService)
+}
