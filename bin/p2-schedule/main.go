95ab25ae37d8f7db89ef319ff7397724d2431726
tummychow <sjung@squareup.com>
2015-01-16 17:08:04 -0800

    Migrate bin/p2-schedule onto pkg/kp

--- bin/p2-schedule/main.go
+++ bin/p2-schedule/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/square/p2/pkg/intent"
+	"github.com/square/p2/pkg/kp"
 	"github.com/square/p2/pkg/pods"
 	"gopkg.in/alecthomas/kingpin.v1"
 )
@@ -19,28 +19,28 @@ func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 
-	store, err := intent.LookupStore(intent.Options{})
-	if err != nil {
-		log.Fatalf("Could not look up intent store: %s", err)
-	}
-	node := *nodeName
-	if node == "" {
-		node, err = os.Hostname()
+	store := kp.NewStore(kp.Options{})
+
+	if *nodeName == "" {
+		hostname, err := os.Hostname()
 		if err != nil {
 			log.Fatalf("Could not get the hostname to do scheduling: %s", err)
 		}
+		*nodeName = hostname
 	}
+
 	if len(*manifests) == 0 {
 		kingpin.Usage()
 		log.Fatalln("No manifests given")
 	}
+
 	for _, manifestPath := range *manifests {
 		manifest, err := pods.PodManifestFromPath(manifestPath)
 		if err != nil {
 			os.Stderr.WriteString(fmt.Sprintf("Could not read manifest at %s: %s\n", manifestPath, err))
 			continue
 		}
-		duration, err := store.SetPod(node, *manifest)
+		duration, err := store.SetPod(kp.IntentPath(*nodeName, manifest.ID()), *manifest)
 		if err != nil {
 			os.Stderr.WriteString(fmt.Sprintf("Could not write manifest %s to intent store: %s\n", manifest.ID(), err))
 			continue
