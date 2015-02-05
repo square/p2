632a13f7728108807a2df9ea30f2d9f6f020cc72
tummychow <sjung@squareup.com>
2015-02-05 14:52:54 -0800

    Add MkdirChownAll test
    Unfortunately chown requires root capabilities and tests aren't always
    running as root, so we can't do much to test the chowning.

---
+++ pkg/util/mkdir_test.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"strconv"
+	"testing"
+
+	. "github.com/anthonybishopric/gotcha"
+)
+
+func TestMkdirAll(t *testing.T) {
+	temp, err := ioutil.TempDir("", "mkdirall")
+	Assert(t).IsNil(err, "There should not have been an error creating a temp dir")
+
+	dirPath := fmt.Sprintf("%s/foo/bar/baz", temp)
+
+	curUser, err := user.Current()
+	Assert(t).IsNil(err, "There should not have been an error checking the current user")
+	uid, err := strconv.ParseInt(curUser.Uid, 10, 0)
+	Assert(t).IsNil(err, "There should not have been an error converting uid to int")
+	gid, err := strconv.ParseInt(curUser.Gid, 10, 0)
+	Assert(t).IsNil(err, "There should not have been an error converting gid to int")
+
+	err = MkdirChownAll(dirPath, int(uid), int(gid), 0777)
+	Assert(t).IsNil(err, "There should not have been an error creating the directory")
+
+	_, err = os.Stat(dirPath)
+	Assert(t).IsNil(err, "There should not have been an error statting the directory")
+}
