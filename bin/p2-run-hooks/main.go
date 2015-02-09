7500e2e75364689f0e2c1c79f68e32d2c5385c46
abishopric <abishopric@squareup.com>
2015-02-09 13:46:44 -0800

    Add p2-run-hooks
    This a debugging utility to make running hooks for apps a little
    easier. This also fixes the integration test, which changed when
    users were given ownership of their pod directory.

---
+++ bin/p2-run-hooks/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"path"
+
+	"github.com/square/p2/pkg/hooks"
+	"github.com/square/p2/pkg/logging"
+	"github.com/square/p2/pkg/pods"
+	"github.com/square/p2/pkg/version"
+	"gopkg.in/alecthomas/kingpin.v1"
+)
+
+var (
+	PodDir    = kingpin.Arg("pod", "A path to a pod that exists on disk already.").Required().String()
+	Lifecycle = kingpin.Arg("hook-type", "Execute one of the given hook types").Required().String()
+	HookDir   = kingpin.Flag("hook-dir", "The root of the hooks").Default(hooks.DEFAULT_PATH).String()
+	Manifest  = kingpin.Flag("manifest", "The manifest to use (this is useful when we are in the before_install phase)").ExistingFile()
+)
+
+func main() {
+	kingpin.Version(version.VERSION)
+	kingpin.Parse()
+
+	dir := hooks.Hooks(*HookDir, &logging.DefaultLogger)
+
+	hookType, err := hooks.AsHookType(*Lifecycle)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	pod := pods.NewPod(path.Base(*PodDir), *PodDir)
+
+	var manifest *pods.PodManifest
+	if *Manifest != "" {
+		manifest, err = pods.PodManifestFromPath(*Manifest)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		manifest, err = pod.CurrentManifest()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	log.Printf("About to run %s hooks for pod %s\n", hookType, pod.Path())
+	err = dir.RunHookType(hookType, pod, manifest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
