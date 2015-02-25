1236a2a1917341ee8f5637ef4af1b71540fb4fb6
tummychow <sjung@squareup.com>
2015-02-24 17:03:40 -0800

    Support HTTP and HTTPS health checks
    As of this commit, HTTPS is now the default

--- pkg/kp/service.go
+++ pkg/kp/service.go
@@ -7,6 +7,12 @@ import (
 	"github.com/square/p2/pkg/pods"
 )
 
+var (
+	// prints the HTTP response on stderr, while exiting 0 if the status code is 200
+	httpStatusCheck  = `if [[ $(curl http://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}") == "200" ]] ; then exit 0 ; else exit 2; fi`
+	httpsStatusCheck = `if [[ $(curl https://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}" --cacert ${SECRETS_PATH}/service2service.ca.pem) == "200" ]] ; then exit 0 ; else exit 2; fi`
+)
+
 // RegisterService creates a consul service for the given pod manifest. If the
 // manifest specifies a status port, the resulting consul service will also
 // include a health check for that port.
@@ -18,10 +24,13 @@ func (s *Store) RegisterService(manifest pods.PodManifest) error {
 	if manifest.StatusPort != 0 {
 		podService.Port = manifest.StatusPort
 		podService.Check = &consulapi.AgentServiceCheck{
-			// prints the HTTP response on stderr, while exiting 0 if the status code is 200
-			Script: fmt.Sprintf(`if [[ $(curl https://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}" --cacert ${SECRETS_PATH}/service2service.ca.pem) == "200" ]] ; then exit 0 ; else exit 2; fi`, manifest.StatusPort),
 			// magic number alert
 			Interval: "5s",
+		}
+		if manifest.StatusHTTP {
+			podService.Check.Script = fmt.Sprintf(httpStatusCheck, manifest.StatusPort)
+		} else {
+			podService.Check.Script = fmt.Sprintf(httpsStatusCheck, manifest.StatusPort)
 		}
 	}
 
