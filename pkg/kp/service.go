906f19f41241b3e81136f615bc4642a6a44e14ba
tummychow <sjung@squareup.com>
2015-02-25 10:26:22 -0800

    Move certificate into preparer config
    It doesn't really belong here either but it's probably better than where
    it was before

--- pkg/kp/service.go
+++ pkg/kp/service.go
@@ -10,13 +10,13 @@ import (
 var (
 	// prints the HTTP response on stderr, while exiting 0 if the status code is 200
 	httpStatusCheck  = `if [[ $(curl http://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}") == "200" ]] ; then exit 0 ; else exit 2; fi`
-	httpsStatusCheck = `if [[ $(curl https://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}" --cacert ${SECRETS_PATH}/service2service.ca.pem) == "200" ]] ; then exit 0 ; else exit 2; fi`
+	httpsStatusCheck = `if [[ $(curl https://$(hostname):%v/_status -s -o /dev/stderr -w "%%{http_code}" --cacert '%s') == "200" ]] ; then exit 0 ; else exit 2; fi`
 )
 
 // RegisterService creates a consul service for the given pod manifest. If the
 // manifest specifies a status port, the resulting consul service will also
 // include a health check for that port.
-func (s *Store) RegisterService(manifest pods.PodManifest) error {
+func (s *Store) RegisterService(manifest pods.PodManifest, caPath string) error {
 	podService := &consulapi.AgentServiceRegistration{
 		Name: manifest.ID(),
 	}
@@ -30,7 +30,7 @@ func (s *Store) RegisterService(manifest pods.PodManifest) error {
 		if manifest.StatusHTTP {
 			podService.Check.Script = fmt.Sprintf(httpStatusCheck, manifest.StatusPort)
 		} else {
-			podService.Check.Script = fmt.Sprintf(httpsStatusCheck, manifest.StatusPort)
+			podService.Check.Script = fmt.Sprintf(httpsStatusCheck, manifest.StatusPort, caPath)
 		}
 	}
 
