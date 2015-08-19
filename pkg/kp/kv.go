square/p2

commit 7181aa854cbd9a4562457c268dbdff7607f15de5

Add extra logging information to log messages that have an 'err' field. This enables another process to consume log messages and publish them to an exception reporting service such as Airbrake or Bugsnag

pkg/kp/kv.go
@@ -6,6 +6,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"path/filepath"
+	"runtime"
 	"time"
 
 	"github.com/hashicorp/consul/api"
@@ -67,14 +69,50 @@ type KVError struct {
 	Op          string
 	Key         string
 	UnsafeError error
+	filename    string
+	function    string
+	lineNumber  int
 }
+
+var _ util.CallsiteError = KVError{}
+
+const KVErrorFormat = "%s failed for path %s%s"
 
 func (err KVError) Error() string {
 	cerr := ""
 	if *showConsulErrors {
 		cerr = fmt.Sprintf(": %s", err.UnsafeError)
 	}
-	return fmt.Sprintf("%s failed for path %s%s", err.Op, err.Key, cerr)
+	return fmt.Sprintf(KVErrorFormat, err.Op, err.Key, cerr)
+}
+
+func (err KVError) Filename() string {
+	return err.filename
+}
+
+func (err KVError) Function() string {
+	return err.function
+}
+
+func (err KVError) LineNumber() int {
+	return err.lineNumber
+}
+
+func NewKVError(op string, key string, unsafeError error) KVError {
+	var function string
+	// Skip one stack frame to get the file & line number of caller.
+	pc, file, line, ok := runtime.Caller(1)
+	if ok {
+		function = runtime.FuncForPC(pc).Name()
+	}
+	return KVError{
+		Op:          op,
+		Key:         key,
+		UnsafeError: unsafeError,
+		filename:    filepath.Base(file),
+		function:    function,
+		lineNumber:  line,
+	}
 }
 
 func (c consulStore) PutHealth(res WatchResult) (time.Time, time.Duration, error) {
@@ -96,7 +134,7 @@ func (c consulStore) PutHealth(res WatchResult) (time.Time, time.Duration, error
 		retDur = writeMeta.RequestTime
 	}
 	if err != nil {
-		return t, retDur, KVError{Op: "put", Key: key, UnsafeError: err}
+		return t, retDur, NewKVError("put", key, err)
 	}
 	return t, retDur, nil
 }

bin/p2-preparer/main.go
@@ -27,12 +27,12 @@ func main() {
 	}
 	preparerConfig, err := preparer.LoadConfig(configPath)
 	if err != nil {
-		logger.WithField("inner_err", err).Fatalln("could not load preparer config")
+		logger.WithError(err).Fatalln("could not load preparer config")
 	}
 
 	prep, err := preparer.New(preparerConfig, logger)
 	if err != nil {
-		logger.WithField("inner_err", err).Fatalln("Could not initialize preparer")
+		logger.WithError(err).Fatalln("Could not initialize preparer")
 	}
 	defer prep.Close()
 

pkg/hooks/hooks.go
@@ -82,8 +82,7 @@ func runDirectory(dirpath string, environment []string, logger logging.Logger) e
 		cmd.Env = environment
 		err := cmd.Run()
 		if err != nil {
-			logger.WithFields(logrus.Fields{
-				"err":    err,
+			logger.WithError(err).WithFields(logrus.Fields{
 				"path":   fullpath,
 				"output": hookOut.String(),
 			}).Warnf("Could not execute hook %s", f.Name())
@@ -114,14 +113,16 @@ func (h *HookDir) runHooks(dirpath string, pod Pod, podManifest *pods.Manifest) 
 	// Write manifest to a file so hooks can read it.
 	tmpManifestFile, err := ioutil.TempFile("", fmt.Sprintf("%s-manifest.yaml", podManifest.Id))
 	if err != nil {
-		logger.WithField("err", err).Warnln("Unable to open manifest file for hooks")
+		logger.WithError(err).
+			Warnln("Unable to open manifest file for hooks")
 		return err
 	}
 	defer os.Remove(tmpManifestFile.Name())
 
 	err = podManifest.Write(tmpManifestFile)
 	if err != nil {
-		logger.WithField("err", err).Warnln("Unable to write manifest file for hooks")
+		logger.WithError(err).
+			Warnln("Unable to write manifest file for hooks")
 		return err
 	}
 

pkg/logging/logging.go
@@ -60,6 +60,21 @@ func (l *Logger) NoFields() *logrus.Entry {
 	return l.WithFields(logrus.Fields{})
 }
 
+func (l *Logger) WithError(err error) *logrus.Entry {
+	fields := logrus.Fields{
+		"err": err.Error(),
+	}
+	// if err conforms to the util.CallsiteError interface, add some fields to
+	// the log message that can be used for exception reporting by a buddy
+	// launchable (e.g. to bugsnag or airbrake)
+	if stackErr, ok := err.(util.CallsiteError); ok {
+		fields["line_number"] = stackErr.LineNumber()
+		fields["filename"] = stackErr.Filename()
+		fields["function"] = stackErr.Function()
+	}
+	return l.Logger.WithFields(fields)
+}
+
 func (l *Logger) WithFields(fields logrus.Fields) *logrus.Entry {
 	return l.Logger.WithFields(Merge(Merge(l.baseFields, fields), processCounter.Fields()))
 }

pkg/logging/logging_test.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"encoding/json"
+	"fmt"
 	"net"
 	"os"
 	"testing"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	. "github.com/anthonybishopric/gotcha"
+	"github.com/square/p2/pkg/util"
 )
 
 func TestLoggingCanMergeFields(t *testing.T) {
@@ -65,6 +67,41 @@ func TestWithFieldsCombinesBaseFieldsAndGiven(t *testing.T) {
 
 	Assert(t).AreEqual("a", entry.Data["foo"], "should have kept foo")
 	Assert(t).AreEqual("c", entry.Data["baz"], "should have merged baz")
+}
+
+type fakeStackError struct {
+	lineNumber int
+	filename   string
+	function   string
+}
+
+func (f *fakeStackError) LineNumber() int {
+	return f.lineNumber
+}
+func (f *fakeStackError) Filename() string {
+	return f.filename
+}
+func (f *fakeStackError) Function() string {
+	return f.function
+}
+func (f *fakeStackError) Error() string {
+	return "error message"
+}
+
+func TestWithError(t *testing.T) {
+	var fakeError util.CallsiteError
+	fakeError = &fakeStackError{
+		lineNumber: 45,
+		filename:   "foo.go",
+		function:   "foo.New",
+	}
+
+	logger := NewLogger(logrus.Fields{})
+	entry := logger.WithError(fakeError)
+
+	Assert(t).AreEqual(entry.Data["line_number"], 45, fmt.Sprintf("Expected line number to be 45, was %d", entry.Data["line_number"]))
+	Assert(t).AreEqual(entry.Data["filename"], "foo.go", fmt.Sprintf("Expected filename to be foo.go, was %s", entry.Data["filename"]))
+	Assert(t).AreEqual(entry.Data["function"], "foo.New", fmt.Sprintf("Expected function to be foo.New, was %s", entry.Data["function"]))
 }
 
 func TestAddSocketHook(t *testing.T) {

pkg/pods/pod.go
@@ -518,16 +518,13 @@ func (pod *Pod) getLaunchable(launchableStanza LaunchableStanza, runAsUser strin
 }
 
 func (p *Pod) logError(err error, message string) {
-	p.logger.WithFields(logrus.Fields{
-		"err": err.Error(),
-	}).Error(message)
+	p.logger.WithError(err).
+		Error(message)
 }
 
 func (p *Pod) logLaunchableError(launchableId string, err error, message string) {
-	p.logger.WithFields(logrus.Fields{
-		"launchable": launchableId,
-		"err":        err.Error(),
-	}).Error(message)
+	p.logger.WithError(err).
+		WithField("launchable", launchableId).Error(message)
 }
 
 func (p *Pod) logInfo(message string) {

pkg/preparer/listener.go
@@ -55,7 +55,7 @@ func (l *HookListener) Sync(quit <-chan struct{}, errCh chan<- error) {
 			watcherQuit <- struct{}{}
 			return
 		case err := <-watcherErrCh:
-			l.Logger.WithField("err", err).Errorln("Error while watching pods")
+			l.Logger.WithError(err).Errorln("Error while watching pods")
 			errCh <- err
 		case result := <-podChan:
 			sub := l.Logger.SubLogger(logrus.Fields{
@@ -78,7 +78,7 @@ func (l *HookListener) Sync(quit <-chan struct{}, errCh chan<- error) {
 			// event is called "before_install"
 			event, err := l.determineEvent(result.Path)
 			if err != nil {
-				sub.WithField("err", err).Errorln("Couldn't determine hook path")
+				sub.WithError(err).Errorln("Couldn't determine hook path")
 				break
 			}
 
@@ -106,14 +106,14 @@ func (l *HookListener) Sync(quit <-chan struct{}, errCh chan<- error) {
 			// The manifest is new, go ahead and install
 			err = hookPod.Install(&result.Manifest)
 			if err != nil {
-				sub.WithField("err", err).Errorln("Could not install hook")
+				sub.WithError(err).Errorln("Could not install hook")
 				errCh <- err
 				break
 			}
 
 			_, err = hookPod.WriteCurrentManifest(&result.Manifest)
 			if err != nil {
-				sub.WithField("err", err).Errorln("Could not write current manifest")
+				sub.WithError(err).Errorln("Could not write current manifest")
 				errCh <- err
 				break
 			}
@@ -121,7 +121,7 @@ func (l *HookListener) Sync(quit <-chan struct{}, errCh chan<- error) {
 			// Now that the pod is installed, link it up to the exec dir.
 			err = l.writeHook(event, hookPod, &result.Manifest)
 			if err != nil {
-				sub.WithField("err", err).Errorln("Could not write hook link")
+				sub.WithError(err).Errorln("Could not write hook link")
 			} else {
 				sub.NoFields().Infoln("Updated hook")
 			}
@@ -165,7 +165,7 @@ func (l *HookListener) writeHook(event string, hookPod *pods.Pod, manifest *pods
 	for _, match := range matches {
 		err = os.Remove(match)
 		if err != nil {
-			l.Logger.WithField("err", err).Warnln("Could not remove old hook")
+			l.Logger.WithError(err).Warnln("Could not remove old hook")
 		}
 	}
 
@@ -189,18 +189,18 @@ func (l *HookListener) writeHook(event string, hookPod *pods.Pod, manifest *pods
 			file, err := os.OpenFile(scriptPath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0744)
 			defer file.Close()
 			if err != nil {
-				l.Logger.WithField("err", err).Errorln("Could not write to event dir path")
+				l.Logger.WithError(err).Errorln("Could not write to event dir path")
 			}
 			err = executable.WriteExecutor(file)
 			if err != nil {
-				l.Logger.WithField("err", err).Errorln("Could not install new hook")
+				l.Logger.WithError(err).Errorln("Could not install new hook")
 			}
 		}
 		// for convenience as we do with regular launchables, make these ones
 		// current under the launchable directory
 		err = launchable.MakeCurrent()
 		if err != nil {
-			l.Logger.WithField("err", err).Errorln("Could not update the current hook")
+			l.Logger.WithError(err).Errorln("Could not update the current hook")
 		}
 	}
 	return nil

pkg/preparer/orchestrate.go
@@ -57,7 +57,7 @@ func (p *Preparer) WatchForHooks(quit chan struct{}) {
 			hookQuitCh <- struct{}{}
 			return
 		case err := <-hookErrCh:
-			p.Logger.WithField("err", err).Errorln("Error updating hooks")
+			p.Logger.WithError(err).Errorln("Error updating hooks")
 		}
 	}
 }
@@ -82,9 +82,8 @@ func (p *Preparer) WatchForPodManifestsForNode(quitAndAck chan struct{}) {
 	for {
 		select {
 		case err := <-errChan:
-			p.Logger.WithFields(logrus.Fields{
-				"inner_err": err,
-			}).Errorln("there was an error reading the manifest")
+			p.Logger.WithError(err).
+				Errorln("there was an error reading the manifest")
 		case result := <-podChan:
 			podId := result.Manifest.ID()
 			if podChanMap[podId] == nil {
@@ -110,10 +109,8 @@ func (p *Preparer) WatchForPodManifestsForNode(quitAndAck chan struct{}) {
 func (p *Preparer) tryRunHooks(hookType hooks.HookType, pod hooks.Pod, manifest *pods.Manifest, logger logging.Logger) {
 	err := p.hooks.RunHookType(hookType, pod, manifest)
 	if err != nil {
-		logger.WithFields(logrus.Fields{
-			"err":   err,
-			"hooks": hookType,
-		}).Warnln("Could not run hooks")
+		logger.WithError(err).
+			WithField("hooks", hookType).Warnln("Could not run hooks")
 	}
 }
 
@@ -197,10 +194,8 @@ func (p *Preparer) installAndLaunchPod(newManifest *pods.Manifest, pod Pod, logg
 	// if the old manifest is corrupted somehow, re-launch since we don't know if this is an update.
 	problemReadingCurrentManifest := (err != nil && err != pods.NoCurrentManifest)
 	if problemReadingCurrentManifest {
-		logger.WithFields(logrus.Fields{
-			"sha":       newSHA,
-			"inner_err": err,
-		}).Errorln("Current manifest not readable, will relaunch")
+		logger.WithError(err).WithField("sha", newSHA).
+			Errorln("Current manifest not readable, will relaunch")
 	}
 
 	if newOrDifferent || problemReadingCurrentManifest {
@@ -209,15 +204,14 @@ func (p *Preparer) installAndLaunchPod(newManifest *pods.Manifest, pod Pod, logg
 		err = pod.Install(newManifest)
 		if err != nil {
 			// install failed, abort and retry
-			logger.WithFields(logrus.Fields{
-				"err": err,
-			}).Errorln("Install failed")
+			logger.WithError(err).Errorln("Install failed")
 			return false
 		}
 
 		err = pod.Verify(newManifest, p.authPolicy)
 		if err != nil {
-			logger.WithField("err", err).Errorln("Pod digest verification failed")
+			logger.WithError(err).
+				Errorln("Pod digest verification failed")
 			p.tryRunHooks(hooks.AFTER_AUTH_FAIL, pod, newManifest, logger)
 			return false
 		}
@@ -227,7 +221,7 @@ func (p *Preparer) installAndLaunchPod(newManifest *pods.Manifest, pod Pod, logg
 		if p.consulHealth {
 			err = p.store.RegisterService(*newManifest, p.caFile)
 			if err != nil {
-				logger.WithField("err", err).Errorln("Service registration failed")
+				logger.WithError(err).Errorln("Service registration failed")
 				return false
 			}
 		}
@@ -235,7 +229,8 @@ func (p *Preparer) installAndLaunchPod(newManifest *pods.Manifest, pod Pod, logg
 		if currentManifest != nil {
 			success, err := pod.Halt(currentManifest)
 			if err != nil {
-				logger.WithField("err", err).Errorln("Pod halt failed")
+				logger.WithError(err).
+					Errorln("Pod halt failed")
 			} else if !success {
 				logger.NoFields().Warnln("One or more launchables did not halt successfully")
 			}
@@ -243,16 +238,13 @@ func (p *Preparer) installAndLaunchPod(newManifest *pods.Manifest, pod Pod, logg
 
 		ok, err := pod.Launch(newManifest)
 		if err != nil {
-			logger.WithFields(logrus.Fields{
-				"err": err,
-			}).Errorln("Launch failed")
+			logger.WithError(err).
+				Errorln("Launch failed")
 		} else {
 			duration, err := p.store.SetPod(realityPath, *newManifest)
 			if err != nil {
-				logger.WithFields(logrus.Fields{
-					"err":      err,
-					"duration": duration,
-				}).Errorln("Could not set pod in reality store")
+				logger.WithError(err).WithField("duration", duration).
+					Errorln("Could not set pod in reality store")
 			}
 
 			p.tryRunHooks(hooks.AFTER_LAUNCH, pod, newManifest, logger)

commit fb95583279eb27fc568438159893a31f17e63793

Save error location in all KVErrors
Commit 7181aa85 introduced the `NewKVError` type constructor for creating
`KVError` values that included the function/file/lineno information for where
the error was created. This commit finishes converting all literal-constructed
`KVError` locations to use the new constructor function.

pkg/kp/kv.go
@@ -76,14 +76,12 @@ type KVError struct {
 
 var _ util.CallsiteError = KVError{}
 
-const KVErrorFormat = "%s failed for path %s%s"
-
 func (err KVError) Error() string {
 	cerr := ""
 	if *showConsulErrors {
 		cerr = fmt.Sprintf(": %s", err.UnsafeError)
 	}
-	return fmt.Sprintf(KVErrorFormat, err.Op, err.Key, cerr)
+	return fmt.Sprintf("%s failed for path %s%s", err.Op, err.Key, cerr)
 }
 
 func (err KVError) Filename() string {
@@ -144,17 +142,17 @@ func (c consulStore) GetHealth(service, node string) (WatchResult, error) {
 	key := HealthPath(service, node)
 	res, _, err := c.client.KV().Get(key, nil)
 	if err != nil {
-		return WatchResult{}, KVError{Op: "get", Key: key, UnsafeError: err}
+		return WatchResult{}, NewKVError("get", key, err)
 	} else if res == nil {
 		return WatchResult{}, nil
 	}
 	err = json.Unmarshal(res.Value, healthRes)
 	if err != nil {
-		return WatchResult{}, KVError{Op: "get", Key: key, UnsafeError: err}
+		return WatchResult{}, NewKVError("get", key, err)
 	}
 	stale := isStale(*healthRes)
 	if stale {
-		return *healthRes, KVError{Op: "get", Key: key, UnsafeError: fmt.Errorf("stale health entry")}
+		return *healthRes, NewKVError("get", key, fmt.Errorf("stale health entry"))
 	}
 	return *healthRes, nil
 }
@@ -164,7 +162,7 @@ func (c consulStore) GetServiceHealth(service string) (map[string]WatchResult, e
 	key := HealthPath(service, "")
 	res, _, err := c.client.KV().List(key, nil)
 	if err != nil {
-		return healthRes, KVError{Op: "list", Key: key, UnsafeError: err}
+		return healthRes, NewKVError("list", key, err)
 	} else if res == nil {
 		return healthRes, nil
 	}
@@ -172,7 +170,7 @@ func (c consulStore) GetServiceHealth(service string) (map[string]WatchResult, e
 		watch := &WatchResult{}
 		err = json.Unmarshal(kvp.Value, watch)
 		if err != nil {
-			return healthRes, KVError{Op: "get", Key: key, UnsafeError: err}
+			return healthRes, NewKVError("get", key, err)
 		}
 		// maps key to result (eg /health/hello/nodename)
 		healthRes[kvp.Key] = *watch
@@ -200,7 +198,7 @@ func (c consulStore) SetPod(key string, manifest pods.Manifest) (time.Duration, 
 		retDur = writeMeta.RequestTime
 	}
 	if err != nil {
-		return retDur, KVError{Op: "put", Key: key, UnsafeError: err}
+		return retDur, NewKVError("put", key, err)
 	}
 	return retDur, nil
 }
@@ -211,7 +209,7 @@ func (c consulStore) SetPod(key string, manifest pods.Manifest) (time.Duration, 
 func (c consulStore) Pod(key string) (*pods.Manifest, time.Duration, error) {
 	kvPair, writeMeta, err := c.client.KV().Get(key, nil)
 	if err != nil {
-		return nil, 0, KVError{Op: "get", Key: key, UnsafeError: err}
+		return nil, 0, NewKVError("get", key, err)
 	}
 	if kvPair == nil {
 		return nil, writeMeta.RequestTime, pods.NoCurrentManifest
@@ -227,7 +225,7 @@ func (c consulStore) Pod(key string) (*pods.Manifest, time.Duration, error) {
 func (c consulStore) ListPods(keyPrefix string) ([]ManifestResult, time.Duration, error) {
 	kvPairs, writeMeta, err := c.client.KV().List(keyPrefix, nil)
 	if err != nil {
-		return nil, 0, KVError{Op: "list", Key: keyPrefix, UnsafeError: err}
+		return nil, 0, NewKVError("list", keyPrefix, err)
 	}
 	var ret []ManifestResult
 
@@ -264,7 +262,7 @@ func (c consulStore) WatchPods(keyPrefix string, quitChan <-chan struct{}, errCh
 				WaitIndex: curIndex,
 			})
 			if err != nil {
-				errChan <- KVError{Op: "list", Key: keyPrefix, UnsafeError: err}
+				errChan <- NewKVError("list", keyPrefix, err)
 			} else {
 				curIndex = meta.LastIndex
 				for _, pair := range pairs {
@@ -295,7 +293,7 @@ func (c consulStore) WatchPods(keyPrefix string, quitChan <-chan struct{}, errCh
 func (c consulStore) Ping() error {
 	_, qm, err := c.client.Catalog().Nodes(&api.QueryOptions{RequireConsistent: true})
 	if err != nil {
-		return KVError{Op: "ping", Key: "/catalog/nodes", UnsafeError: err}
+		return NewKVError("ping", "/catalog/nodes", err)
 	}
 	if qm == nil || !qm.KnownLeader {
 		return util.Errorf("No known leader")

pkg/kp/lock.go
@@ -49,7 +49,7 @@ func (c consulStore) NewLock(name string) (Lock, error) {
 func (c consulStore) LockHolder(key string) (string, string, error) {
 	kvp, _, err := c.client.KV().Get(key, nil)
 	if err != nil {
-		return "", "", KVError{Op: "get", Key: key, UnsafeError: err}
+		return "", "", NewKVError("get", key, err)
 	}
 	if kvp == nil || kvp.Session == "" {
 		return "", "", nil
@@ -78,7 +78,7 @@ func (l Lock) Lock(key string) error {
 	}, nil)
 
 	if err != nil {
-		return KVError{Op: "acquire lock", Key: key, UnsafeError: err}
+		return NewKVError("acquire lock", key, err)
 	}
 	if success {
 		return nil