fc681e91607fc004531d4d30fc5d6fc8bb273281
tummychow <sjung@squareup.com>
2015-08-31 14:15:41 -0700

    Label storage implementation
    Started by @anthonybishopric. This code provides a generalized interface
    for storing labels (maps of strings to strings) on "things" (namely pods
    and nodes), and a consul-backed implementation of that interface.
    
    The implementation stores labels as JSON objects under the path
    /labels/<type>/<id>. To safely mutate the object, CAS operations are
    necessary, so the implementation supports automatic retries for CAS
    failure. Tests included.

---
+++ pkg/labels/consul_applicator.go
@@ -0,0 +1,177 @@
+package labels
+
+import (
+	"encoding/json"
+	"fmt"
+	"path"
+
+	"github.com/square/p2/Godeps/_workspace/src/github.com/hashicorp/consul/api"
+	klabels "github.com/square/p2/Godeps/_workspace/src/k8s.io/kubernetes/pkg/labels"
+	"github.com/square/p2/pkg/logging"
+)
+
+const labelRoot = "labels"
+
+type CASError struct {
+	Key string
+}
+
+func (e CASError) Error() string {
+	return fmt.Sprintf("Could not compare-and-set key %q", e.Key)
+}
+
+type consulKV interface {
+	List(prefix string, opts *api.QueryOptions) (api.KVPairs, *api.QueryMeta, error)
+	CAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error)
+	DeleteCAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error)
+	Get(key string, q *api.QueryOptions) (*api.KVPair, *api.QueryMeta, error)
+}
+
+type consulApplicator struct {
+	kv      consulKV
+	logger  logging.Logger
+	retries int
+}
+
+func NewConsulApplicator(client *api.Client, retries int) *consulApplicator {
+	return &consulApplicator{
+		logger:  logging.DefaultLogger,
+		kv:      client.KV(),
+		retries: retries,
+	}
+}
+
+func objectPath(labelType Type, id string) string {
+	return path.Join(labelRoot, labelType.String(), id)
+}
+
+func (c *consulApplicator) getLabels(labelType Type, id string) (Labeled, uint64, error) {
+	kvp, _, err := c.kv.Get(objectPath(labelType, id), nil)
+	if err != nil || kvp == nil {
+		return Labeled{
+			ID:        id,
+			LabelType: labelType,
+			Labels:    klabels.Set{},
+		}, 0, err
+	}
+
+	l, err := convertKVPToLabeled(kvp)
+	return l, kvp.ModifyIndex, err
+}
+func (c *consulApplicator) GetLabels(labelType Type, id string) (Labeled, error) {
+	l, _, err := c.getLabels(labelType, id)
+	return l, err
+}
+
+func (c *consulApplicator) GetMatches(selector klabels.LabelSelector, labelType Type) ([]Labeled, error) {
+	// TODO: Label selector result caching
+	allMatches, _, err := c.kv.List(path.Join(labelRoot, labelType.String()), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []Labeled{}
+	for _, kvp := range allMatches {
+		l, err := convertKVPToLabeled(kvp)
+		if err != nil {
+			return res, err
+		}
+		if selector.Matches(l.Labels) {
+			res = append(res, l)
+		}
+	}
+	return res, nil
+}
+
+// generalized label mutator function - pass nil value to delete that label
+func (c *consulApplicator) mutateLabel(labelType Type, id, label string, value *string) error {
+	l, index, err := c.getLabels(labelType, id)
+	if err != nil {
+		return err
+	}
+
+	if value == nil {
+		delete(l.Labels, label)
+	} else {
+		l.Labels[label] = *value
+	}
+	setkvp, err := convertLabeledToKVP(l)
+	if err != nil {
+		return err
+	}
+	setkvp.ModifyIndex = index
+
+	var success bool
+	if len(l.Labels) == 0 {
+		// still have to use CAS when deleting, to avoid discarding someone
+		// else's concurrent modification
+		// DeleteCAS ignores the value on the KVPair, so it doesn't matter if
+		// we set it earlier
+		success, _, err = c.kv.DeleteCAS(setkvp, nil)
+	} else {
+		success, _, err = c.kv.CAS(setkvp, nil)
+	}
+	if err != nil {
+		return err
+	}
+	if !success {
+		return CASError{setkvp.Key}
+	}
+	return nil
+}
+
+// this function will attempt to mutateLabel. if it gets a CAS error, then it
+// will retry up to the number of attempts specified in c.Retries
+func (c *consulApplicator) retryMutate(labelType Type, id, label string, value *string) error {
+	err := c.mutateLabel(labelType, id, label, value)
+	for i := 0; i < c.retries; i++ {
+		if _, ok := err.(CASError); ok {
+			err = c.mutateLabel(labelType, id, label, value)
+		} else {
+			break
+		}
+	}
+	return err
+}
+
+func (c *consulApplicator) SetLabel(labelType Type, id, label, value string) error {
+	return c.retryMutate(labelType, id, label, &value)
+}
+
+func (c *consulApplicator) RemoveLabel(labelType Type, id, label string) error {
+	return c.retryMutate(labelType, id, label, nil)
+}
+
+// kvp must be non-nil
+func convertKVPToLabeled(kvp *api.KVPair) (Labeled, error) {
+	ret := Labeled{
+		ID:     path.Base(kvp.Key),
+		Labels: klabels.Set{},
+	}
+
+	// /<root>/<type>/<id>
+	typeStr := path.Base(path.Dir(kvp.Key))
+	labelType, err := AsType(typeStr)
+	if err != nil {
+		return ret, err
+	}
+	ret.LabelType = labelType
+
+	err = json.Unmarshal(kvp.Value, &ret.Labels)
+	return ret, err
+}
+
+func convertLabeledToKVP(l Labeled) (*api.KVPair, error) {
+	value, err := json.Marshal(l.Labels)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.KVPair{
+		Key:   objectPath(l.LabelType, l.ID),
+		Value: value,
+	}, nil
+}
+
+// confirm at compile time that consulApplicator is an implementation of the Applicator interface
+var _ Applicator = &consulApplicator{}
---
+++ pkg/labels/consul_applicator_test.go
@@ -0,0 +1,164 @@
+package labels
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	. "github.com/square/p2/Godeps/_workspace/src/github.com/anthonybishopric/gotcha"
+	"github.com/square/p2/Godeps/_workspace/src/github.com/hashicorp/consul/api"
+	klabels "github.com/square/p2/Godeps/_workspace/src/k8s.io/kubernetes/pkg/labels"
+	kutil "github.com/square/p2/Godeps/_workspace/src/k8s.io/kubernetes/pkg/util"
+	"github.com/square/p2/pkg/logging"
+)
+
+type fakeLabelStore struct {
+	data map[string][]byte
+}
+
+func (f *fakeLabelStore) List(prefix string, opts *api.QueryOptions) (api.KVPairs, *api.QueryMeta, error) {
+	var ret api.KVPairs
+
+	for k, v := range f.data {
+		if strings.HasPrefix(k, prefix) {
+			ret = append(ret, &api.KVPair{
+				Key:   k,
+				Value: v,
+			})
+		}
+	}
+
+	return ret, &api.QueryMeta{}, nil
+}
+
+func (f *fakeLabelStore) DeleteCAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error) {
+	delete(f.data, pair.Key)
+	return true, &api.WriteMeta{}, nil
+}
+
+func (f *fakeLabelStore) CAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error) {
+	f.data[pair.Key] = pair.Value
+	return true, &api.WriteMeta{}, nil
+}
+
+func (f *fakeLabelStore) Get(key string, q *api.QueryOptions) (*api.KVPair, *api.QueryMeta, error) {
+	if v, ok := f.data[key]; ok {
+		return &api.KVPair{
+			Key:   key,
+			Value: v,
+		}, nil, nil
+	} else {
+		return nil, nil, nil
+	}
+}
+
+func TestBasicSetGetRemove(t *testing.T) {
+	c := &consulApplicator{
+		kv:     &fakeLabelStore{data: map[string][]byte{}},
+		logger: logging.DefaultLogger,
+	}
+
+	Assert(t).IsNil(c.SetLabel(POD, "object", "label", "value"), "should have had nil error when setting label")
+
+	labeledObject, err := c.GetLabels(POD, "object")
+	Assert(t).IsNil(err, "should have had nil error when getting labels")
+	Assert(t).IsTrue(reflect.DeepEqual(labeledObject.Labels, klabels.Set{
+		"label": "value",
+	}), "should have had matching label map {\"label\": \"value\"}")
+
+	Assert(t).IsNil(c.RemoveLabel(POD, "object", "label"), "should have had nil error when clearing label")
+
+	labeledObject, err = c.GetLabels(POD, "object")
+	Assert(t).IsNil(err, "should have had nil error when getting labels")
+	Assert(t).IsTrue(reflect.DeepEqual(labeledObject.Labels, klabels.Set{}), "should have had empty label map")
+}
+
+func TestEmptyGetRemove(t *testing.T) {
+	c := &consulApplicator{
+		kv:     &fakeLabelStore{data: map[string][]byte{}},
+		logger: logging.DefaultLogger,
+	}
+
+	labeledObject, err := c.GetLabels(POD, "object")
+	Assert(t).IsNil(err, "should have had nil error when getting labels")
+	Assert(t).IsTrue(reflect.DeepEqual(labeledObject.Labels, klabels.Set{}), "should have had empty label map")
+
+	Assert(t).IsNil(c.RemoveLabel(POD, "object", "label"), "no error when removing nonexistent label or object")
+}
+
+func TestBasicMatch(t *testing.T) {
+	c := &consulApplicator{
+		kv:     &fakeLabelStore{data: map[string][]byte{}},
+		logger: logging.DefaultLogger,
+	}
+
+	Assert(t).IsNil(c.SetLabel(POD, "object", "label", "value"), "should have had nil error when setting label")
+
+	req, err := klabels.NewRequirement("label", klabels.EqualsOperator, kutil.NewStringSet("value"))
+	Assert(t).IsNil(err, "should have had nil error creating positive requirement")
+	matches, err := c.GetMatches(klabels.LabelSelector{*req}, POD)
+	Assert(t).IsNil(err, "should have had nil error fetching positive matches")
+	Assert(t).AreEqual(len(matches), 1, "should have had exactly one positive match")
+
+	matches, err = c.GetMatches(klabels.LabelSelector{*req}, NODE)
+	Assert(t).IsNil(err, "should have had nil error fetching positive matches for wrong type")
+	Assert(t).AreEqual(len(matches), 0, "should have had exactly zero mistyped matches")
+
+	req, err = klabels.NewRequirement("label", klabels.NotInOperator, kutil.NewStringSet("value"))
+	Assert(t).IsNil(err, "should have had nil error creating negative requirement")
+	matches, err = c.GetMatches(klabels.LabelSelector{*req}, POD)
+	Assert(t).IsNil(err, "should have had nil error fetching negative matches")
+	Assert(t).AreEqual(len(matches), 0, "should have had exactly zero negative matches")
+}
+
+type failOnceLabelStore struct {
+	inner      consulKV
+	succeedCAS bool
+}
+
+func (f *failOnceLabelStore) List(prefix string, opts *api.QueryOptions) (api.KVPairs, *api.QueryMeta, error) {
+	return f.inner.List(prefix, opts)
+}
+
+func (f *failOnceLabelStore) DeleteCAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error) {
+	if !f.succeedCAS {
+		f.succeedCAS = true
+		return false, &api.WriteMeta{}, nil
+	}
+	return f.inner.DeleteCAS(pair, opts)
+}
+
+func (f *failOnceLabelStore) CAS(pair *api.KVPair, opts *api.WriteOptions) (bool, *api.WriteMeta, error) {
+	if !f.succeedCAS {
+		f.succeedCAS = true
+		return false, &api.WriteMeta{}, nil
+	}
+	return f.inner.CAS(pair, opts)
+}
+
+func (f *failOnceLabelStore) Get(key string, q *api.QueryOptions) (*api.KVPair, *api.QueryMeta, error) {
+	return f.inner.Get(key, q)
+}
+
+func TestCASRetries(t *testing.T) {
+	c := &consulApplicator{
+		kv:      &failOnceLabelStore{inner: &fakeLabelStore{data: map[string][]byte{}}},
+		logger:  logging.DefaultLogger,
+		retries: 3,
+	}
+
+	Assert(t).IsNil(c.SetLabel(POD, "object", "label", "value"), "should have retried despite failing once")
+}
+
+func TestCASNoRetries(t *testing.T) {
+	c := &consulApplicator{
+		kv:      &failOnceLabelStore{inner: &fakeLabelStore{data: map[string][]byte{}}},
+		logger:  logging.DefaultLogger,
+		retries: 0,
+	}
+
+	err := c.SetLabel(POD, "object", "label", "value")
+	Assert(t).IsNotNil(err, "should have failed on first try")
+	_, ok := err.(CASError)
+	Assert(t).IsTrue(ok, "should have returned a CASError")
+}
