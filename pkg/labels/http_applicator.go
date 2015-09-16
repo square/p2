ee43392a09a00ad06bb8ea78647b625d0d51eb9a
Peter Tseng <ptseng@squareup.com>
2015-09-15 21:08:29 -0700

    Add HTTP Applicator and tests
    This allows using an HTTP server that accepts GET requests and returns
    JSON lists of strings representing the nodes that match the query.

---
+++ pkg/labels/http_applicator.go
@@ -0,0 +1,85 @@
+package labels
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/square/p2/pkg/logging"
+	"github.com/square/p2/pkg/util"
+)
+
+type httpApplicator struct {
+	client *http.Client
+	// The endpoint that will be queried for matches.
+	// GetMatches will add to this endpoint a query parameter with key "selector" and value selector.String()
+	matchesEndpoint *url.URL
+	logger          logging.Logger
+}
+
+var _ Applicator = &httpApplicator{}
+
+func NewHttpApplicator(client *http.Client, matchesEndpoint *url.URL) (*httpApplicator, error) {
+	if matchesEndpoint == nil {
+		return nil, util.Errorf("matches endpoint cannot be nil")
+	}
+
+	c := client
+	if c == nil {
+		c = http.DefaultClient
+	}
+
+	return &httpApplicator{
+		logger:          logging.DefaultLogger,
+		matchesEndpoint: matchesEndpoint,
+		client:          c,
+	}, nil
+}
+
+func (h *httpApplicator) SetLabel(labelType Type, id, name, value string) error {
+	return util.Errorf("SetLabel not implemented for HttpApplicator (type %s, id %s, name %s, value %s)", labelType, id, name, value)
+}
+
+func (h *httpApplicator) RemoveLabel(labelType Type, id, name string) error {
+	return util.Errorf("RemoveLabel not implemented for HttpApplicator (type %s, id %s, name %s)", labelType, id, name)
+}
+
+func (h *httpApplicator) GetLabels(labelType Type, id string) (Labeled, error) {
+	return Labeled{}, util.Errorf("GetLabels not implemented for HttpApplicator (type %s, id %s)", labelType, id)
+}
+
+func (h *httpApplicator) GetMatches(selector Selector, labelType Type) ([]Labeled, error) {
+	params := url.Values{}
+	params.Add("selector", selector.String())
+	params.Add("type", labelType.String())
+
+	// Make value copy of URL; don't want to mutate the URL in the struct.
+	urlToGet := *h.matchesEndpoint
+	urlToGet.RawQuery = params.Encode()
+
+	resp, err := h.client.Get(urlToGet.String())
+	if err != nil {
+		return []Labeled{}, err
+	}
+
+	defer resp.Body.Close()
+
+	matches := []string{}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&matches)
+	if err != nil {
+		return []Labeled{}, err
+	}
+
+	labeled := make([]Labeled, len(matches))
+
+	for i, s := range matches {
+		labeled[i] = Labeled{
+			ID:        s,
+			LabelType: labelType,
+			Labels:    Set{},
+		}
+	}
+
+	return labeled, nil
+}
