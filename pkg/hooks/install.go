2c6b07d47dcfe4d3c167b9b1c2fef1921cf34bd1
tummychow <sjung@squareup.com>
2015-09-29 12:27:16 -0700

    Move hook script writing to dedicated function
    This will be useful for some binary utilities outside pkg/preparer. We
    should make it generally available.

---
+++ pkg/hooks/install.go
@@ -0,0 +1,72 @@
+package hooks
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/square/p2/Godeps/_workspace/src/github.com/Sirupsen/logrus"
+	"github.com/square/p2/pkg/logging"
+	"github.com/square/p2/pkg/pods"
+	"github.com/square/p2/pkg/runit"
+	"github.com/square/p2/pkg/util"
+)
+
+// Populates the given directory with executor scripts for each launch script of
+// the given pod, which must be installed. Any orphaned executor scripts (from a
+// past install, but no longer present in this pod) will be cleaned out.
+func InstallHookScripts(dir string, hookPod *pods.Pod, manifest pods.Manifest, logger logging.Logger) error {
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// TODO: globbing based on the structure of the name is gross, each hook pod
+	// should have its own dir of scripts and running hooks should iterate over
+	// the directories
+	rmPattern := filepath.Join(dir, fmt.Sprintf("%s__*", hookPod.Id))
+	matches, err := filepath.Glob(rmPattern)
+	if err != nil {
+		// error return from filepath.Glob is guaranteed to be ErrBadPattern
+		return util.Errorf("error while removing old hook scripts: pattern %q is malformed", rmPattern)
+	}
+	for _, match := range matches {
+		err = os.Remove(match)
+		if err != nil {
+			logger.WithErrorAndFields(err, logrus.Fields{"script_path": match}).Errorln("Could not remove old hook script")
+		}
+	}
+
+	launchables, err := hookPod.Launchables(manifest)
+	if err != nil {
+		return err
+	}
+	for _, launchable := range launchables {
+		executables, err := launchable.Executables(runit.DefaultBuilder)
+		if err != nil {
+			return err
+		}
+
+		for _, executable := range executables {
+			// Write a script to the event directory that executes the pod's executables
+			// with the correct environment for that pod.
+			scriptPath := filepath.Join(dir, executable.Service.Name)
+			file, err := os.OpenFile(scriptPath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0744)
+			defer file.Close()
+			if err != nil {
+				logger.WithErrorAndFields(err, logrus.Fields{"script_path": scriptPath}).Errorln("Could not open new hook script")
+			}
+			err = executable.WriteExecutor(file)
+			if err != nil {
+				logger.WithErrorAndFields(err, logrus.Fields{"script_path": scriptPath}).Errorln("Could not write new hook script")
+			}
+		}
+		// for convenience as we do with regular launchables, make these ones
+		// current under the launchable directory
+		err = launchable.MakeCurrent()
+		if err != nil {
+			logger.WithErrorAndFields(err, logrus.Fields{"launchable_id": launchable.Id}).Errorln("Could not set hook launchable to current")
+		}
+	}
+	return nil
+}
