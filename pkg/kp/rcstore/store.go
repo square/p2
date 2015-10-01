fd1f91366a8a86faf54517055c2f211ef3840e61
tummychow <sjung@squareup.com>
2015-10-01 14:53:47 -0700

    Identify an RC's nodes using its ID label
    This change is necessary so that two RCs can have the same set of pod
    labels without colliding.

--- pkg/kp/rcstore/store.go
+++ pkg/kp/rcstore/store.go
@@ -11,11 +11,7 @@ import (
 type Store interface {
 	// Create creates a replication controller with the specified manifest and selectors.
 	// The node selector is used to determine what nodes the replication controller may schedule on.
-	// The pod label set is:
-	// 1) applied to every pod the replication controller schedules,
-	// 2) used to determine what pods are already scheduled.
-	// Note that this implies that creating two replication controllers with the same podLabels
-	// means that the two will see each others' pods.
+	// The pod label set is applied to every pod the replication controller schedules.
 	Create(manifest pods.Manifest, nodeSelector labels.Selector, podLabels labels.Set) (fields.RC, error)
 
 	Get(id fields.ID) (fields.RC, error)
