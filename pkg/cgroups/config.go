5b5c004795878298ab0866e1017416ac74c69f88
abishopric <abishopric@squareup.com>
2015-12-05 11:28:55 -0800

    Allow shorthand for memory limits.
    This change allows manifest authors to specify a shorthand for
    the byte limits of their cgroups. Previously, all `memory` entries
    for the `cgroup` stanza had to be in the form of an integer
    number of bytes. Authors may now use a shorthand as defined by
    the package `github.com/square/p2/pkg/util/size`.
    
    An author wishing to limit their launchable to a maximum resident
    memory limit of 4 Gibibytes may use the shorthand `4G` instead of
    `4294967296`.
    
    The old form of specifying an integer limit of bytes is also supported.

--- pkg/cgroups/config.go
+++ pkg/cgroups/config.go
@@ -2,12 +2,28 @@ package cgroups
 
 import (
 	"fmt"
+
+	"github.com/square/p2/pkg/util/size"
 )
 
 type Config struct {
 	Name   string `yaml:"-"`                // The name of the cgroup in cgroupfs
 	CPUs   int    `yaml:"cpus,omitempty"`   // The number of logical CPUs
-	Memory int    `yaml:"memory,omitempty"` // The number of bytes of memory
+	Memory string `yaml:"memory,omitempty"` // The number of bytes of memory
+}
+
+func NewCGroup(cpus int, memory size.ByteCount) Config {
+	return Config{
+		CPUs:   cpus,
+		Memory: memory.String(),
+	}
+}
+
+func (config Config) MemoryByteCount() (size.ByteCount, error) {
+	if config.Memory == "" {
+		return size.Byte * 0, nil
+	}
+	return size.Parse(config.Memory)
 }
 
 func (config Config) CgexecArgs() []string {
