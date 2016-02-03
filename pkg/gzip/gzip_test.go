29dbd6804518fcfac420a850a5d6785a8e1d330c
Peter Tseng <ptseng@squareup.com>
2016-02-03 14:42:54 -0800

    gzip: Error on escaping symlinks
    Consider a tarball with `a/b` being extracted to `/my/jail`. Now let's
    consider the case where `/my/jail/a` is a symlink pointing to `..` or
    even `/usr` (this may happen through a previous entry in the same
    tarball, a previous tarball, or any variety of reasons).
    
    If we do not check symlinks, we may blindly extract `b` to a path
    outside of `/my/jail`. We should disallow this, as this sort of path
    traversal can result in unsafe behavior (modification of system files).
    
    Behavior of BSD tar: Forbids extracting such a tarball unless
    specifically directed to do so with the `-P` flag.
    
    Behavior of GNU tar: Extracts normally, such that you get `/my/b`
    or `/usr/b` extracted, escaping the jail.
    
    We will err on the side of caution because we may extract files as a
    privileged user. Therefore we will raise an error in this case.

--- pkg/gzip/gzip_test.go
+++ pkg/gzip/gzip_test.go
@@ -34,9 +34,12 @@ func testExtractionWithSetup(t *testing.T, tarfile string,
 	user, err := user.Current()
 	Assert(t).IsNil(err, "expected no error getting current user")
 
-	err = ExtractTarGz(user.Username, file, dest)
+	realDest, err := filepath.EvalSymlinks(dest)
+	Assert(t).IsNil(err, "expected no error evaluating symlinks")
 
-	check(err, dest)
+	err = ExtractTarGz(user.Username, file, realDest)
+
+	check(err, realDest)
 }
 
 func testExtraction(t *testing.T, tarfile string, check func(error, string)) {
@@ -58,5 +61,62 @@ func TestPathWithParent(t *testing.T) {
 
 		_, err := os.Stat(filepath.Join(dest, "..", "b"))
 		Assert(t).IsTrue(os.IsNotExist(err), "expected extracted file not to exist")
+	})
+}
+
+func testSymlink(t *testing.T, linkTarget func(string) string, check func(error, string)) {
+	testExtractionWithSetup(t, "file_without_dir.tar.gz",
+		func(dest string) error {
+			return os.Symlink(linkTarget(dest), filepath.Join(dest, "a"))
+		},
+		check,
+	)
+}
+
+func testEscapingSymlink(t *testing.T, linkTarget func(string) string) {
+	testSymlink(t, linkTarget, func(tarErr error, dest string) {
+		Assert(t).IsNotNil(tarErr, "expected error extracting tarball")
+
+		_, err := os.Stat(filepath.Join(dest, "..", "b", "c"))
+		Assert(t).IsTrue(os.IsNotExist(err), "expected extracted file not to exist")
+	})
+}
+
+func testNonEscapingSymlink(t *testing.T, linkTarget func(string) string) {
+	testSymlink(t, linkTarget, func(tarErr error, dest string) {
+		Assert(t).IsNil(tarErr, "expected no error extracting tarball")
+
+		_, err := os.Stat(filepath.Join(dest, "b", "c"))
+		Assert(t).IsNil(err, "expected no error statting extracted file")
+	})
+}
+
+func TestEscapingAbsoluteSymlink(t *testing.T) {
+	testEscapingSymlink(t, func(dest string) string {
+		return filepath.Dir(dest)
+	})
+}
+
+func TestEscapingAbsoluteSymlink2(t *testing.T) {
+	testEscapingSymlink(t, func(dest string) string {
+		return "/"
+	})
+}
+
+func TestEscapingRelativeSymlink(t *testing.T) {
+	testEscapingSymlink(t, func(_dest string) string {
+		return ".."
+	})
+}
+
+func TestNonEscapingAbsoluteSymlink(t *testing.T) {
+	testNonEscapingSymlink(t, func(dest string) string {
+		return dest
+	})
+}
+
+func TestNonEscapingRelativeSymlink(t *testing.T) {
+	testNonEscapingSymlink(t, func(_dest string) string {
+		return "."
 	})
 }
