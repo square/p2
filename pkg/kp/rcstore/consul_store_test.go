a0b3b16852bae9f0ab99bd9779dfaf8d5cd879bd
Michael Puncel <mpuncel@squareup.com>
2016-03-29 14:54:29 -0700

    add tests for failure and exit scenarios of publishLatestRCs

--- pkg/kp/rcstore/consul_store_test.go
+++ pkg/kp/rcstore/consul_store_test.go
@@ -84,6 +84,171 @@ func TestPublishLatestRCs(t *testing.T) {
 	}
 }
 
+func TestPublishLatestRCsSkipsIfCorrupt(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRCs(inCh, quitCh)
+
+	// push some legitimate RCs and read them out
+	var val []fields.RC
+	inCh <- rcsWithIDs(t, "a", 3)
+	select {
+	case val = <-outCh:
+	case err := <-errCh:
+		t.Fatalf("Unexpected error on errCh: %s", err)
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, rc := range val {
+		if rc.ID.String() != "a" {
+			t.Errorf("Expected all RCs to have id %s, was %s", "a", rc.ID)
+		}
+	}
+
+	// Now push some bogus JSON that will trigger an error
+	corruptData := []*api.KVPair{&api.KVPair{Value: []byte("bad_json")}}
+	inCh <- corruptData
+
+	select {
+	case val = <-outCh:
+		t.Fatalf("Didn't expect out value for bogus input")
+	case <-errCh:
+		// good
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out reading from channel")
+	}
+
+	// Now push more legitimate stuff and make sure that is written
+	// push some legitimate RCs and read them out
+	inCh <- rcsWithIDs(t, "b", 3)
+	select {
+	case val = <-outCh:
+	case err := <-errCh:
+		t.Fatalf("Unexpected error on errCh: %s", err)
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, rc := range val {
+		if rc.ID.String() != "b" {
+			t.Errorf("Expected all RCs to have id %s, was %s", "b", rc.ID)
+		}
+	}
+}
+
+func TestPublishQuitsOnQuitChannelClose(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+
+	outCh, errCh := publishLatestRCs(inCh, quitCh)
+	close(quitCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since quitCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since quitCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
+func TestPublishQuitsOnInChannelCloseBeforeData(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRCs(inCh, quitCh)
+	close(inCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
+func TestPublishQuitsOnInChannelCloseAfterData(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRCs(inCh, quitCh)
+
+	// Write some legitimate data and read it out
+	var val []fields.RC
+	// Put some values on the inCh and read them from outCh transformed
+	// into RCs
+	inCh <- rcsWithIDs(t, "a", 3)
+	select {
+	case val = <-outCh:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, rc := range val {
+		if rc.ID.String() != "a" {
+			t.Errorf("Expected all RCs to have id %s, was %s", "a", rc.ID)
+		}
+	}
+
+	close(inCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
 func rcsWithIDs(t *testing.T, id string, num int) api.KVPairs {
 	var pairs api.KVPairs
 	builder := pods.NewManifestBuilder()
