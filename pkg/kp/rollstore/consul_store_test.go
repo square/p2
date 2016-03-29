1d7a6936608fca2f5dc6ba602ae6dc9ea7bfd5fc
Michael Puncel <mpuncel@squareup.com>
2016-03-29 14:58:27 -0700

    add test cases for failure and exit scenarios of publishLatestRolls

--- pkg/kp/rollstore/consul_store_test.go
+++ pkg/kp/rollstore/consul_store_test.go
@@ -861,6 +861,171 @@ func TestPublishLatestRolls(t *testing.T) {
 	}
 }
 
+func TestPublishLatestRCsSkipsIfCorrupt(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRolls(inCh, quitCh)
+
+	// push some legitimate RCs and read them out
+	var val []fields.Update
+	inCh <- rollsWithIDs(t, "a", 3)
+	select {
+	case val = <-outCh:
+	case err := <-errCh:
+		t.Fatalf("Unexpected error on errCh: %s", err)
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, ru := range val {
+		if ru.ID().String() != "a" {
+			t.Errorf("Expected all RUs to have id %s, was %s", "a", ru.ID)
+		}
+	}
+
+	// Now push some bogus JSON that will trigger an error
+	corruptData := []*api.KVPair{&api.KVPair{Value: []byte("bad_json")}}
+	inCh <- corruptData
+
+	select {
+	case val = <-outCh:
+		t.Fatalf("Didn't expect out value for bogus input")
+	case <-errCh:
+		// good
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out reading from channel")
+	}
+
+	// Now push more legitimate stuff and make sure that is written
+	// push some legitimate RUs and read them out
+	inCh <- rollsWithIDs(t, "b", 3)
+	select {
+	case val = <-outCh:
+	case err := <-errCh:
+		t.Fatalf("Unexpected error on errCh: %s", err)
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, ru := range val {
+		if ru.ID().String() != "b" {
+			t.Errorf("Expected all RUs to have id %s, was %s", "b", ru.ID)
+		}
+	}
+}
+
+func TestPublishQuitsOnQuitChannelClose(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+
+	outCh, errCh := publishLatestRolls(inCh, quitCh)
+	close(quitCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since quitCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since quitCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
+func TestPublishQuitsOnInChannelCloseBeforeData(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRolls(inCh, quitCh)
+	close(inCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
+func TestPublishQuitsOnInChannelCloseAfterData(t *testing.T) {
+	inCh := make(chan api.KVPairs)
+	quitCh := make(chan struct{})
+	defer close(quitCh)
+
+	outCh, errCh := publishLatestRolls(inCh, quitCh)
+
+	// Write some legitimate data and read it out
+	var val []fields.Update
+	// Put some values on the inCh and read them from outCh transformed
+	// into RUs
+	inCh <- rollsWithIDs(t, "a", 3)
+	select {
+	case val = <-outCh:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("timed out reading from channel")
+	}
+
+	if len(val) != 3 {
+		t.Errorf("Expected %d values on outCh, got %d", 3, len(val))
+	}
+
+	for _, ru := range val {
+		if ru.ID().String() != "a" {
+			t.Errorf("Expected all RUs to have id %s, was %s", "a", ru.ID)
+		}
+	}
+
+	close(inCh)
+
+	select {
+	case _, ok := <-outCh:
+		if ok {
+			t.Fatalf("outCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for outCh to close")
+	}
+
+	select {
+	case _, ok := <-errCh:
+		if ok {
+			t.Fatalf("errCh should have closed since inCh closed")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Timed out waiting for errCh to close")
+	}
+}
+
 func rollsWithIDs(t *testing.T, id string, num int) api.KVPairs {
 	var pairs api.KVPairs
 	for i := 0; i < num; i++ {
