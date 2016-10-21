9949d8cf6dcd2ecd045d4a66ca09b3095c3838d8
Michael Puncel <mpuncel@squareup.com>
2016-10-21 15:19:44 -0700

    Add grpc endpoint for WatchMatches()
    It serves the same purpose and closely models the signature of
    WatchMatches() on labels.Applicator.

---
+++ bin/grpc/servers/label-store-server/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"github.com/square/p2/pkg/grpc/label_store"
+	label_protos "github.com/square/p2/pkg/grpc/label_store/protos"
+	"github.com/square/p2/pkg/labels"
+	"github.com/square/p2/pkg/logging"
+	"github.com/square/p2/pkg/store/consul"
+	"github.com/square/p2/pkg/store/consul/flags"
+
+	"github.com/Sirupsen/logrus"
+	"google.golang.org/grpc"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	verbose = kingpin.Flag("verbose", "Enable verbose logging for the server").Bool()
+)
+
+type config struct {
+	Port int `yaml:"port"`
+}
+
+const defaultPort = 3000
+
+func main() {
+	// Parse custom flags + standard Consul routing options
+	_, opts, _ := flags.ParseWithConsulOptions()
+
+	logrusLogger := logging.DefaultLogger
+	if *verbose {
+		logrusLogger.Logger.Level = logrus.DebugLevel
+	}
+	client := consul.NewConsulClient(opts)
+	applicator := labels.NewConsulApplicator(client, 1)
+
+	logger := log.New(os.Stderr, "", 0)
+	port := getPort(logger)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		logger.Fatalf("failed to listen: %v", err)
+	}
+
+	logrusLogger.Infof("Listening tcp on port %d", port)
+	s := grpc.NewServer()
+	label_protos.RegisterLabelStoreServer(s, label_store.NewServer(applicator, logrusLogger))
+	if err := s.Serve(lis); err != nil {
+		logger.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func getPort(logger *log.Logger) int {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		return defaultPort
+	}
+
+	configBytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	var config config
+	err = yaml.Unmarshal(configBytes, &config)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	if config.Port == 0 {
+		return defaultPort
+	}
+
+	return config.Port
+}
