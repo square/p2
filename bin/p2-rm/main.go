94e1f1c42f04410df620f677976d47a7390996d5
Michael Puncel <mpuncel@squareup.com>
2016-11-07 12:51:06 -0800

    Add support for uuid pods to p2-rm.
    When deleting a uuid pod, the --pod-unique-key key must be used and the
    --node flag must not be used.
    
    --pod is still required because it is a good way to verify that the pod
    being deleted is the correct one, however it isn't strictly necessary
    since the pod uuid is the only necessary information to look up and
    unschedule a pod.

--- bin/p2-rm/main.go
+++ bin/p2-rm/main.go
@@ -5,17 +5,13 @@ import (
 	"fmt"
 	"os"
 	"os/user"
-	"path"
-	"time"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/square/p2/pkg/kp"
 	"github.com/square/p2/pkg/kp/consulutil"
 	"github.com/square/p2/pkg/kp/flags"
-	"github.com/square/p2/pkg/kp/rcstore"
 	"github.com/square/p2/pkg/labels"
-	"github.com/square/p2/pkg/rc"
 	"github.com/square/p2/pkg/rc/fields"
 	"github.com/square/p2/pkg/types"
 	"github.com/square/p2/pkg/version"
@@ -24,88 +20,68 @@ import (
 // Command line flags
 var (
 	podName      = kingpin.Arg("pod", "The names of the pod to be removed").Required().String()
-	nodeName     = kingpin.Flag("node", "The node to do the scheduling on. Uses the hostname by default.").String()
+	nodeName     = kingpin.Flag("node", "The node to unschedule the pod from. Uses the hostname by default. Only applies to \"legacy\" pods.").String()
+	podUniqueKey = kingpin.Flag("pod-unique-key", "The pod unique key to unschedule. Only applies to \"uuid\" pods. Cannot be used with --node").Short('k').String()
 	deallocation = kingpin.Flag("deallocate", "Specifies that we are deallocating this pod on this node. Using this switch will mutate the desired_replicas value on a managing RC, if one exists.").Bool()
 )
-
-type store interface {
-	NewSession(name string, renewalCh <-chan time.Time) (kp.Session, chan error, error)
-	DeletePod(podPrefix kp.PodPrefix, nodename types.NodeName, podId types.PodID) (time.Duration, error)
-}
-
-// P2RM contains the state necessary to safely remove a pod from a node
-type P2RM struct {
-	Store   store
-	RCStore rcstore.Store
-	Client  consulutil.ConsulClient
-	Labeler labels.ApplicatorWithoutWatches
-
-	LabelID  string
-	NodeName types.NodeName
-	PodName  string
-}
-
-// NewP2RM is a constructor for the P2RM type. It will generate the necessary
-// storage types based on its api.Client argument
-func NewP2RM(client consulutil.ConsulClient, podName string, nodeName types.NodeName, labeler labels.ApplicatorWithoutWatches) *P2RM {
-	rm := &P2RM{}
-	rm.Client = client
-	rm.Store = kp.NewConsulStore(client)
-	rm.RCStore = rcstore.NewConsul(client, labeler, 5)
-	rm.Labeler = labeler
-	rm.LabelID = path.Join(nodeName.String(), podName)
-	rm.PodName = podName
-	rm.NodeName = nodeName
-
-	return rm
-}
 
 func main() {
 	kingpin.Version(version.VERSION)
 	_, opts, labeler := flags.ParseWithConsulOptions()
 
-	if *nodeName == "" {
-		hostname, err := os.Hostname()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error getting hostname. use --node to specify a node: %v\n", err)
-			os.Exit(1)
-		}
-		*nodeName = hostname
-	}
+	consulClient := kp.NewConsulClient(opts)
 
-	rm := NewP2RM(kp.NewConsulClient(opts), *podName, types.NodeName(*nodeName), labeler)
+	err := handlePodRemoval(consulClient, labeler)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func handlePodRemoval(consulClient consulutil.ConsulClient, labeler labels.ApplicatorWithoutWatches) error {
+	var rm *P2RM
+	if *podUniqueKey != "" {
+		rm = NewUUIDP2RM(consulClient, types.PodUniqueKey(*podUniqueKey), types.PodID(*podName), labeler)
+	} else {
+		if *nodeName == "" {
+			hostname, err := os.Hostname()
+			if err != nil {
+				return fmt.Errorf("error getting hostname. use --node to specify a node: %v\n", err)
+			}
+			*nodeName = hostname
+		}
+
+		rm = NewLegacyP2RM(consulClient, types.PodID(*podName), types.NodeName(*nodeName), labeler)
+	}
 
 	podIsManagedByRC, rcID, err := rm.checkForManagingReplicationController()
 	if err != nil {
-		os.Exit(2)
+		return err
 	}
 
 	if !podIsManagedByRC {
 		err = rm.deletePod()
 		if err != nil {
-			os.Exit(2)
+			return err
 		}
 	}
 
 	if podIsManagedByRC && !*deallocation {
-		fmt.Fprintf(
-			os.Stderr,
-			"error: %s is managed by replication controller: %s\n"+
-				"It's possible you meant you deallocate this pod on this node. If so, please confirm your intention with --deallocate\n", *nodeName, rcID)
-		os.Exit(2)
+		return fmt.Errorf("error: %s is managed by replication controller: %s\n"+
+			"It's possible you meant you deallocate this pod on this node. If so, please confirm your intention with --deallocate\n", *nodeName, rcID)
 	}
 
 	if podIsManagedByRC && *deallocation {
 		err = rm.decrementDesiredCount(rcID)
 		if err != nil {
-			fmt.Fprintf(os.Stderr,
-				"Encountered error deallocating from the RC %s. You may attempt this command again or use `p2-rctl` to cleanup manually.\n%v",
+			return fmt.Errorf("Encountered error deallocating from the RC %s. You may attempt this command again or use `p2-rctl` to cleanup manually.\n%v",
 				rcID,
 				err)
 		}
 	}
 
-	fmt.Printf("%s: successfully removed %s\n", rm.NodeName, rm.PodName)
+	fmt.Printf("%s: successfully removed %s\n", rm.NodeName, rm.PodID)
+	return nil
 }
 
 func sessionName(rcID fields.ID) string {
@@ -116,91 +92,4 @@ func sessionName(rcID fields.ID) string {
 	}
 
 	return fmt.Sprintf("p2-rm:user:%s:rcID:%s", username, rcID)
-}
-
-func (rm *P2RM) decrementDesiredCount(id fields.ID) error {
-	session, renewalErrChan, err := rm.Store.NewSession(sessionName(id), nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to get consul session. Halting \n%v\n", err)
-		os.Exit(1)
-	}
-
-	go func(errChan chan error) {
-		select {
-		case <-errChan:
-			fmt.Fprintf(os.Stderr, "Got renewal error, proceeding without a lock\n%v\n", err)
-		default:
-		}
-	}(renewalErrChan)
-
-	rcLock, err := rm.RCStore.LockForMutation(id, session)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to lock RC for mutation. Halting.\n %v", err)
-		os.Exit(1)
-	}
-	defer rcLock.Unlock()
-
-	err = rm.RCStore.Disable(id)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not disable RC. %s \n %v", id, err)
-		os.Exit(1)
-	}
-
-	err = rm.deletePod()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to delete pod. Please re-run this command to clean up. \n%v", err)
-		os.Exit(1)
-	}
-
-	err = rm.RCStore.AddDesiredReplicas(id, -1)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to decrement RC count: %v", err)
-		os.Exit(1)
-	}
-
-	err = rm.RCStore.Enable(id)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not enable RC. %s \n %v", id, err)
-		os.Exit(1)
-	}
-
-	if err = session.Destroy(); err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to destroy consul session. %v", err)
-	}
-
-	return nil
-}
-
-func (rm *P2RM) checkForManagingReplicationController() (bool, fields.ID, error) {
-	podLabels, err := rm.Labeler.GetLabels(labels.POD, rm.LabelID)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to check node for labels: %v\n", err)
-		return false, "", err
-	}
-
-	if podLabels.Labels.Has(rc.RCIDLabel) {
-		return true, fields.ID(podLabels.Labels.Get(rc.RCIDLabel)), nil
-	}
-
-	return false, "", nil
-}
-
-func (rm *P2RM) deletePod() error {
-	_, err := rm.Store.DeletePod(kp.INTENT_TREE, rm.NodeName, types.PodID(rm.PodName))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to remove pod: %v", err)
-		return err
-	}
-
-	err = rm.Labeler.RemoveAllLabels(labels.POD, rm.LabelID)
-	if err != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"node is partially deleted. re-run command to finish deleting\n"+
-				"unable to remove pod labels: %v",
-			err,
-		)
-		return err
-	}
-	return nil
 }
