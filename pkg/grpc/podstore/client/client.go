fb2c3c27266b1f8042c76010209abaa07a4b9e7c
Michael Puncel <mpuncel@squareup.com>
2016-12-06 14:48:31 -0500

    Add WatchStatus() function to podstore grpc client

--- pkg/grpc/podstore/client/client.go
+++ pkg/grpc/podstore/client/client.go
@@ -1,10 +1,10 @@
 package client
 
 import (
-	"crypto/tls"
 	"time"
 
 	podstore_protos "github.com/square/p2/pkg/grpc/podstore/protos"
+	"github.com/square/p2/pkg/kp"
 	"github.com/square/p2/pkg/logging"
 	"github.com/square/p2/pkg/manifest"
 	"github.com/square/p2/pkg/types"
@@ -22,10 +22,10 @@ type Client struct {
 	logger logging.Logger
 }
 
-func New(grpcAddress string, tlsConfig *tls.Config, logger logging.Logger) (Client, error) {
+func New(grpcAddress string, creds credentials.TransportCredentials, logger logging.Logger) (Client, error) {
 	dialOptions := []grpc.DialOption{grpc.WithBlock(), grpc.WithTimeout(5 * time.Second)}
-	if tlsConfig != nil {
-		dialOptions = append(dialOptions, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	if creds != nil {
+		dialOptions = append(dialOptions, grpc.WithTransportCredentials(creds))
 	} else {
 		dialOptions = append(dialOptions, grpc.WithInsecure())
 	}
@@ -60,6 +60,39 @@ func (c Client) Schedule(manifest manifest.Manifest, node types.NodeName) (types
 	}
 
 	return types.PodUniqueKey(resp.PodUniqueKey), nil
+}
+
+type PodStatusResult struct {
+	PodStatus *podstore_protos.PodStatusResponse `json:"pod_status"`
+	Error     error                              `json:"-"`
+}
+
+func (c Client) WatchStatus(ctx context.Context, podUniqueKey types.PodUniqueKey, waitIndex uint64) (<-chan PodStatusResult, error) {
+	stream, err := c.client.WatchPodStatus(ctx, &podstore_protos.WatchPodStatusRequest{
+		PodUniqueKey:    podUniqueKey.String(),
+		StatusNamespace: kp.PreparerPodStatusNamespace.String(),
+		WaitIndex:       waitIndex,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	outCh := make(chan PodStatusResult)
+	go func() {
+		defer close(outCh)
+		for {
+			status, err := stream.Recv()
+			select {
+			case <-ctx.Done():
+				return
+			case outCh <- PodStatusResult{
+				PodStatus: status,
+				Error:     err,
+			}:
+			}
+		}
+	}()
+	return outCh, nil
 }
 
 func (c Client) Close() {
