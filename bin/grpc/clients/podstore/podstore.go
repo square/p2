fa71322aa8cbbfc3659cf3d84cced2f3e8ccf8a6
Michael Puncel <mpuncel@squareup.com>
2016-12-06 15:18:12 -0500

    Add ability to watch pod status to podstore grpc client binary

--- bin/grpc/clients/podstore/podstore.go
+++ bin/grpc/clients/podstore/podstore.go
@@ -9,21 +9,29 @@ import (
 	"github.com/square/p2/pkg/manifest"
 	"github.com/square/p2/pkg/types"
 
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/credentials"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 const (
-	cmdScheduleText = "schedule"
-	defaultAddress  = "localhost:3000"
+	cmdScheduleText    = "schedule"
+	cmdWatchStatusText = "watch-status"
+
+	defaultAddress = "localhost:3000"
 )
 
 var (
-	address      = kingpin.Flag("address", "Address of the pod store server to talk to.").Default(defaultAddress).String()
+	address = kingpin.Flag("address", "Address of the pod store server to talk to.").Default(defaultAddress).String()
+	caCert  = kingpin.Flag("cacert", "Certificate file to use to verify server").ExistingFile()
+
 	cmdSchedule  = kingpin.Command(cmdScheduleText, "Schedules a pod (as a UUID pod)")
 	manifestFile = cmdSchedule.Flag("manifest", "Path to pod manifest file to schedule").Required().ExistingFile()
 	node         = cmdSchedule.Flag("node", "Node to schedule pod manifest to").Required().String()
-	insecure     = kingpin.Flag("insecure", "Don't use TLS/SSL to contact store").Bool()
-	caCert       = kingpin.Flag("cacert", "Certificate file to use to verify server").ExistingFile()
+
+	cmdWatchStatus = kingpin.Command(cmdWatchStatusText, "Watch the status for a pod")
+	podUniqueKey   = cmdWatchStatus.Flag("pod-unique-key", "Pod unique key (uuid) to watch status for").Short('k').Required().String()
+	numIterations  = cmdWatchStatus.Flag("num-iterations", "Number of status updates to wait for before stopping").Short('n').Default("1").Int()
 )
 
 func main() {
@@ -31,7 +39,16 @@ func main() {
 
 	logger := logging.DefaultLogger
 
-	client, err := client.New(*address, nil, logger)
+	var creds credentials.TransportCredentials
+	var err error
+	if *caCert != "" {
+		creds, err = credentials.NewClientTLSFromFile(*caCert, "")
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+
+	client, err := client.New(*address, creds, logger)
 	if err != nil {
 		logger.Fatalf("Could not set up grpc client: %s", err)
 	}
@@ -39,6 +56,8 @@ func main() {
 	switch cmd {
 	case cmdScheduleText:
 		schedule(client, logger)
+	case cmdWatchStatusText:
+		watchStatus(client, logger)
 	}
 }
 
@@ -68,4 +87,36 @@ func schedule(client client.Client, logger logging.Logger) {
 	}
 
 	fmt.Println(string(outBytes))
+}
+
+func watchStatus(client client.Client, logger logging.Logger) {
+	key, err := types.ToPodUniqueKey(*podUniqueKey)
+	if err != nil {
+		logger.Fatalf("Could not parse passed pod unique key %q as uuid: %s", *podUniqueKey, err)
+	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	outCh, err := client.WatchStatus(ctx, key, 1) // 1 so we wait for the key to exist
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	for i := 0; i < *numIterations; i++ {
+		val, ok := <-outCh
+		if !ok {
+			logger.Fatal("Channel closed unexpectedly")
+		}
+
+		if val.Error != nil {
+			logger.Fatal(val.Error)
+		}
+
+		bytes, err := json.Marshal(val)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		fmt.Println(string(bytes))
+	}
 }
