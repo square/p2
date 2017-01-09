53f32cc2298ecd24f8e2eeb2c59cfcbabe71c065
Benjamin Wester <bwester@bwester.org>
2017-01-09 19:15:23 +0000

    Update golang.org/x/crypto to c3b1d0d6

--- vendor/golang.org/x/crypto/ssh/server.go
+++ vendor/golang.org/x/crypto/ssh/server.go
@@ -242,7 +242,7 @@ func checkSourceAddress(addr net.Addr, sourceAddr string) error {
 	}
 
 	if allowedIP := net.ParseIP(sourceAddr); allowedIP != nil {
-		if bytes.Equal(allowedIP, tcpAddr.IP) {
+		if allowedIP.Equal(tcpAddr.IP) {
 			return nil
 		}
 	} else {
@@ -284,7 +284,6 @@ userAuthLoop:
 		switch userAuthReq.Method {
 		case "none":
 			if config.NoClientAuth {
-				s.user = ""
 				authErr = nil
 			}
 		case "password":
