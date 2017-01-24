aef783fc37183af4a495b1205d88b2a38ba3dcbe
Michael Puncel <mpuncel@squareup.com>
2017-01-23 17:14:52 -0800

    rake sync_consul_deps

--- vendor/github.com/mitchellh/reflectwalk/reflectwalk.go
+++ vendor/github.com/mitchellh/reflectwalk/reflectwalk.go
@@ -5,7 +5,6 @@
 package reflectwalk
 
 import (
-	"errors"
 	"reflect"
 )
 
@@ -17,12 +16,6 @@ import (
 // structures (slices, maps, etc.) that are walkable by other interfaces.
 type PrimitiveWalker interface {
 	Primitive(reflect.Value) error
-}
-
-// InterfaceWalker implementations are able to handle interface values as they
-// are encountered during the walk.
-type InterfaceWalker interface {
-	Interface(reflect.Value) error
 }
 
 // MapWalker implementations are able to handle individual elements
@@ -37,13 +30,6 @@ type MapWalker interface {
 type SliceWalker interface {
 	Slice(reflect.Value) error
 	SliceElem(int, reflect.Value) error
-}
-
-// ArrayWalker implementations are able to handle array elements found
-// within complex structures.
-type ArrayWalker interface {
-	Array(reflect.Value) error
-	ArrayElem(int, reflect.Value) error
 }
 
 // StructWalker is an interface that has methods that are called for
@@ -69,13 +55,6 @@ type PointerWalker interface {
 	PointerExit(bool) error
 }
 
-// SkipEntry can be returned from walk functions to skip walking
-// the value of this field. This is only valid in the following functions:
-//
-//   - StructField: skips walking the struct value
-//
-var SkipEntry = errors.New("skip this entry")
-
 // Walk takes an arbitrary value and an interface and traverses the
 // value, calling callbacks on the interface if they are supported.
 // The interface should implement one or more of the walker interfaces
@@ -100,63 +79,23 @@ func Walk(data, walker interface{}) (err error) {
 
 func walk(v reflect.Value, w interface{}) (err error) {
 	// Determine if we're receiving a pointer and if so notify the walker.
-	// The logic here is convoluted but very important (tests will fail if
-	// almost any part is changed). I will try to explain here.
-	//
-	// First, we check if the value is an interface, if so, we really need
-	// to check the interface's VALUE to see whether it is a pointer.
-	//
-	// Check whether the value is then a pointer. If so, then set pointer
-	// to true to notify the user.
-	//
-	// If we still have a pointer or an interface after the indirections, then
-	// we unwrap another level
-	//
-	// At this time, we also set "v" to be the dereferenced value. This is
-	// because once we've unwrapped the pointer we want to use that value.
 	pointer := false
-	pointerV := v
-
-	for {
-		if pointerV.Kind() == reflect.Interface {
-			if iw, ok := w.(InterfaceWalker); ok {
-				if err = iw.Interface(pointerV); err != nil {
-					return
-				}
-			}
-
-			pointerV = pointerV.Elem()
+	if v.Kind() == reflect.Ptr {
+		pointer = true
+		v = reflect.Indirect(v)
+	}
+	if pw, ok := w.(PointerWalker); ok {
+		if err = pw.PointerEnter(pointer); err != nil {
+			return
 		}
 
-		if pointerV.Kind() == reflect.Ptr {
-			pointer = true
-			v = reflect.Indirect(pointerV)
-		}
-		if pw, ok := w.(PointerWalker); ok {
-			if err = pw.PointerEnter(pointer); err != nil {
+		defer func() {
+			if err != nil {
 				return
 			}
 
-			defer func(pointer bool) {
-				if err != nil {
-					return
-				}
-
-				err = pw.PointerExit(pointer)
-			}(pointer)
-		}
-
-		if pointer {
-			pointerV = v
-		}
-		pointer = false
-
-		// If we still have a pointer or interface we have to indirect another level.
-		switch pointerV.Kind() {
-		case reflect.Ptr, reflect.Interface:
-			continue
-		}
-		break
+			err = pw.PointerExit(pointer)
+		}()
 	}
 
 	// We preserve the original value here because if it is an interface
@@ -185,9 +124,6 @@ func walk(v reflect.Value, w interface{}) (err error) {
 		return
 	case reflect.Struct:
 		err = walkStruct(v, w)
-		return
-	case reflect.Array:
-		err = walkArray(v, w)
 		return
 	default:
 		panic("unsupported type: " + k.String())
@@ -296,49 +232,6 @@ func walkSlice(v reflect.Value, w interface{}) (err error) {
 	return nil
 }
 
-func walkArray(v reflect.Value, w interface{}) (err error) {
-	ew, ok := w.(EnterExitWalker)
-	if ok {
-		ew.Enter(Array)
-	}
-
-	if aw, ok := w.(ArrayWalker); ok {
-		if err := aw.Array(v); err != nil {
-			return err
-		}
-	}
-
-	for i := 0; i < v.Len(); i++ {
-		elem := v.Index(i)
-
-		if aw, ok := w.(ArrayWalker); ok {
-			if err := aw.ArrayElem(i, elem); err != nil {
-				return err
-			}
-		}
-
-		ew, ok := w.(EnterExitWalker)
-		if ok {
-			ew.Enter(ArrayElem)
-		}
-
-		if err := walk(elem, w); err != nil {
-			return err
-		}
-
-		if ok {
-			ew.Exit(ArrayElem)
-		}
-	}
-
-	ew, ok = w.(EnterExitWalker)
-	if ok {
-		ew.Exit(Array)
-	}
-
-	return nil
-}
-
 func walkStruct(v reflect.Value, w interface{}) (err error) {
 	ew, ewok := w.(EnterExitWalker)
 	if ewok {
@@ -358,12 +251,6 @@ func walkStruct(v reflect.Value, w interface{}) (err error) {
 
 		if sw, ok := w.(StructWalker); ok {
 			err = sw.StructField(sf, f)
-
-			// SkipEntry just pretends this field doesn't even exist
-			if err == SkipEntry {
-				continue
-			}
-
 			if err != nil {
 				return
 			}
