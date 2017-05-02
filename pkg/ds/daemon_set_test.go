c674d0a69a411bf3d06262ad7818e12a7379d687
Michael Puncel <mpuncel@squareup.com>
2017-05-02 10:12:17 -0700

    fix test shutdown race in pkg/ds
    There was a race where a channel is closed at the end of a test while a
    goroutine it spawned might be trying to write to that channel. This
    makes sure that the goroutines using the channels are done before
    closing them.

--- pkg/ds/daemon_set_test.go
+++ pkg/ds/daemon_set_test.go
@@ -206,11 +206,36 @@ func TestSchedule(t *testing.T) {
 	quitCh := make(chan struct{})
 	updatedCh := make(chan *ds_fields.DaemonSet)
 	deletedCh := make(chan *ds_fields.DaemonSet)
-	defer close(quitCh)
-	defer close(updatedCh)
-	defer close(deletedCh)
 	desiresErrCh := ds.WatchDesires(quitCh, updatedCh, deletedCh)
 	dsChangesErrCh := watchDSChanges(ds, dsStore, quitCh, updatedCh, deletedCh)
+
+	// We need to be careful when shutting down in order to avoid data
+	// races, since updatedCh and deletedCh are shared between two
+	// functions (WatchDesires() and watchDSChanges()). We need to make
+	// sure that we don't close updatedCh or deletedCh until their
+	// respective output channels have closed which indicates that they
+	// won't try to send any more values
+	defer func() {
+		close(quitCh)
+		desiresErrChClosed := false
+		dsChangesErrChClosed := false
+		for !dsChangesErrChClosed || !desiresErrChClosed {
+			select {
+			case _, ok := <-dsChangesErrCh:
+				if !ok {
+					dsChangesErrChClosed = true
+				}
+			case _, ok := <-desiresErrCh:
+				if !ok {
+					desiresErrChClosed = true
+				}
+			case <-time.After(1 * time.Second):
+				t.Fatal("watchDSChanges or WatchDesires did not exit promptly after closing quitCh")
+			}
+		}
+		close(updatedCh)
+		close(deletedCh)
+	}()
 
 	//
 	// Verify that the pod has been labeled
@@ -419,11 +444,36 @@ func TestPublishToReplication(t *testing.T) {
 	quitCh := make(chan struct{})
 	updatedCh := make(chan *ds_fields.DaemonSet)
 	deletedCh := make(chan *ds_fields.DaemonSet)
-	defer close(quitCh)
-	defer close(updatedCh)
-	defer close(deletedCh)
 	desiresErrCh := ds.WatchDesires(quitCh, updatedCh, deletedCh)
 	dsChangesErrCh := watchDSChanges(ds, dsStore, quitCh, updatedCh, deletedCh)
+
+	// We need to be careful when shutting down in order to avoid data
+	// races, since updatedCh and deletedCh are shared between two
+	// functions (WatchDesires() and watchDSChanges()). We need to make
+	// sure that we don't close updatedCh or deletedCh until their
+	// respective output channels have closed which indicates that they
+	// won't try to send any more values
+	defer func() {
+		close(quitCh)
+		desiresErrChClosed := false
+		dsChangesErrChClosed := false
+		for !dsChangesErrChClosed || !desiresErrChClosed {
+			select {
+			case _, ok := <-dsChangesErrCh:
+				if !ok {
+					dsChangesErrChClosed = true
+				}
+			case _, ok := <-desiresErrCh:
+				if !ok {
+					desiresErrChClosed = true
+				}
+			case <-time.After(1 * time.Second):
+				t.Fatal("watchDSChanges or WatchDesires did not exit promptly after closing quitCh")
+			}
+		}
+		close(updatedCh)
+		close(deletedCh)
+	}()
 
 	//
 	// Verify that 2 pods have been labeled
