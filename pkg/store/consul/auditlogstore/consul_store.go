e8022a1f24a0200f0d9cc14fb6cd0d6f8367d598
Michael Puncel <mpuncel@squareup.com>
2017-05-03 16:39:55 -0400

    add List() function to audit log store

--- pkg/store/consul/auditlogstore/consul_store.go
+++ pkg/store/consul/auditlogstore/consul_store.go
@@ -3,6 +3,7 @@ package auditlogstore
 import (
 	"encoding/json"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/square/p2/pkg/audit"
@@ -22,6 +23,17 @@ const auditLogTree string = "audit_logs"
 // to a consul client to create records, it will simply save them to a
 // transaction object.
 type ConsulStore struct {
+	consulKV ConsulKV
+}
+
+type ConsulKV interface {
+	List(prefix string, opts *api.QueryOptions) (api.KVPairs, *api.QueryMeta, error)
+}
+
+func NewConsulStore(consulKV ConsulKV) ConsulStore {
+	return ConsulStore{
+		consulKV: consulKV,
+	}
 }
 
 func (ConsulStore) Create(
@@ -59,6 +71,56 @@ func (ConsulStore) Delete(
 	})
 }
 
+func (c ConsulStore) List() (map[audit.ID]audit.AuditLog, error) {
+	pairs, _, err := c.consulKV.List(auditLogTree+"/", nil)
+	if err != nil {
+		return nil, util.Errorf("could not list audit log records: %s", err)
+	}
+
+	ret := make(map[audit.ID]audit.AuditLog)
+	for _, pair := range pairs {
+		id, err := idFromKey(pair.Key)
+		if err != nil {
+			return nil, err
+		}
+
+		al, err := auditLogFromPair(pair)
+		if err != nil {
+			return nil, util.Errorf("could not convert value of audit log %s to audit log struct: %s", pair.Key, err)
+		}
+		ret[id] = al
+	}
+
+	return ret, nil
+}
+
+func auditLogFromPair(pair *api.KVPair) (audit.AuditLog, error) {
+	var al audit.AuditLog
+	err := json.Unmarshal(pair.Value, &al)
+	if err != nil {
+		return audit.AuditLog{}, err
+	}
+
+	return al, nil
+}
+
 func computeKey(id audit.ID) string {
 	return path.Join(auditLogTree, id.String())
+}
+
+func idFromKey(key string) (audit.ID, error) {
+	parts := strings.Split(key, "/")
+	if len(parts) != 2 {
+		return "", util.Errorf("%s did not match expected key format", key)
+	}
+
+	if parts[0] != auditLogTree {
+		return "", util.Errorf("%s did not match expected key format", key)
+	}
+
+	if uuid.Parse(parts[1]) == nil {
+		return "", util.Errorf("%s from audit log key %s did not parse as a UUID", parts[1], key)
+	}
+
+	return audit.ID(parts[1]), nil
 }
