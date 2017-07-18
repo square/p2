a35ceb81a1bd6777c7d8b5a3e31b2748d7494c47
Sean Sorrell <ssorrell@squareup.com>
2017-07-18 16:27:15 -0700

    Effectively revert a change made in #890
    The type of map used by pkg/manifest is map[interface{}]interface{} so it
    behooves us to match that. The map[string]interface{} change was to support JSON
    marshalling of config stanzas, which seems less important now than it once did.

--- pkg/store/consul/configstore/store_test.go
+++ pkg/store/consul/configstore/store_test.go
@@ -22,7 +22,7 @@ type FakeConsulKV struct {
 	config map[ID][]byte
 }
 
-func (kv *FakeConsulKV) insertConfig(id ID, m map[string]interface{}) {
+func (kv *FakeConsulKV) insertConfig(id ID, m map[interface{}]interface{}) {
 	if kv.config == nil {
 		kv.config = make(map[ID][]byte)
 	}
@@ -59,7 +59,7 @@ func (kv *FakeConsulKV) DeleteCAS(p *api.KVPair, q *api.WriteOptions) (bool, *ap
 	return true, nil, nil
 }
 
-func yamlMarshal(m map[string]interface{}) string {
+func yamlMarshal(m map[interface{}]interface{}) string {
 	bs, err := yaml.Marshal(m)
 	if err != nil {
 		panic(fmt.Sprintf("I need better YAML, y'all: %v", err))
@@ -80,7 +80,7 @@ func TestFetchConfig(t *testing.T) {
 	fakeConsulKV := FakeConsulKV{}
 	consulStore := NewConsulStore(&fakeConsulKV, labels.NewFakeApplicator())
 
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	id := ID("foo")
 	fakeConsulKV.insertConfig(id, m)
@@ -115,7 +115,7 @@ func TestPutConfig(t *testing.T) {
 	consulStore := NewConsulStore(&fakeConsulKV, labels.NewFakeApplicator())
 
 	id := ID("foo")
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	f := Fields{
 		ID:     id,
@@ -149,7 +149,7 @@ func TestDeleteConfig(t *testing.T) {
 	consulStore := NewConsulStore(&fakeConsulKV, labels.NewFakeApplicator())
 
 	id := ID("foo")
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	f := Fields{
 		ID:     id,
@@ -174,7 +174,7 @@ func TestLabels(t *testing.T) {
 	consulStore := NewConsulStore(&fakeConsulKV, labels.NewFakeApplicator())
 
 	id := ID("foo")
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	f := Fields{
 		ID:     id,
@@ -210,7 +210,7 @@ func TestPutConfigTxn(t *testing.T) {
 	consulStore := NewConsulStore(fixture.Client.KV(), labels.NewFakeApplicator())
 
 	id := ID("foo")
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	f := Fields{
 		ID:     id,
@@ -251,7 +251,7 @@ func TestDeleteConfigTxn(t *testing.T) {
 	consulStore := NewConsulStore(fixture.Client.KV(), labels.NewFakeApplicator())
 
 	id := ID("foo")
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	m["configuration"] = "hell yeah"
 	f := Fields{
 		ID:     id,
