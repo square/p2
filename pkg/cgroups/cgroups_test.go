3143743b94e66bbd32f4f2984f0af693266da911
sean sorrell <seansorrell@gmail.com>
2018-01-11 15:56:07 -0800

    Extract cgroup subsystem finding, add PodCgroup support functions

---
+++ pkg/cgroups/cgroups_test.go
@@ -0,0 +1,63 @@
+package cgroups
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/square/p2/pkg/types"
+	"github.com/square/p2/pkg/util/size"
+)
+
+type FakeSubsystemer struct {
+	tmpdir string
+}
+
+func (fs *FakeSubsystemer) Find() (Subsystems, error) {
+	var err error
+	if fs.tmpdir == "" {
+		if fs.createTmpdir(); err != nil {
+			return Subsystems{}, err
+		}
+	}
+
+	if err = os.Chmod(fs.tmpdir, os.ModePerm); err != nil {
+		return Subsystems{}, err
+	}
+	return Subsystems{CPU: filepath.Join(fs.tmpdir, "cpu"), Memory: filepath.Join(fs.tmpdir, "memory")}, nil
+}
+
+// createTmpDir is useful only for tests, it creates the tmpdir
+func (fs *FakeSubsystemer) createTmpdir() error {
+	tmpdir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+	fs.tmpdir = tmpdir
+	return nil
+}
+
+func TestCreatePodCgroup(t *testing.T) {
+	c := Config{CPUs: 2, Memory: size.ByteCount(1024)}
+	podID := types.PodID("podID")
+	hostname := types.NodeName("abc123.example")
+	fs := &FakeSubsystemer{}
+	if err := CreatePodCgroup(podID, hostname, c, fs); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	expectCgroupFileToContain(t, "2048\n", filepath.Join(fs.tmpdir, "memory", "p2", hostname.String(), podID.String(), "memory.limit_in_bytes"))
+	expectCgroupFileToContain(t, "2048\n", filepath.Join(fs.tmpdir, "memory", "p2", hostname.String(), podID.String(), "memory.memsw.limit_in_bytes"))
+	expectCgroupFileToContain(t, "1024\n", filepath.Join(fs.tmpdir, "memory", "p2", hostname.String(), podID.String(), "memory.soft_limit_in_bytes"))
+}
+
+func expectCgroupFileToContain(t *testing.T, s string, file string) {
+	actual, err := ioutil.ReadFile(filepath.Join(file))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if string(actual) != s { // We set the hard limit to 2x the request
+		t.Errorf("expected %s, but got: %s", s, string(actual))
+	}
+}
--- pkg/cgroups/config.go
+++ pkg/cgroups/config.go
@@ -4,8 +4,9 @@ import (
 	"github.com/square/p2/pkg/util/size"
 )
 
+// Config combines an ID and subsystem limits
 type Config struct {
-	Name   string         `yaml:"-"`                // The name of the cgroup in cgroupfs
+	Name   CgroupID       `yaml:"-"`                // The name of the cgroup in cgroupfs
 	CPUs   int            `yaml:"cpus,omitempty"`   // The number of logical CPUs
 	Memory size.ByteCount `yaml:"memory,omitempty"` // The number of bytes of memory
 }
