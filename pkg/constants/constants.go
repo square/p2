575dff33b26b8309ef0cb2a3db4ab4b553ac4abb
Zongkun Yang <yangzongkun@squareup.com>
2018-06-21 01:45:41 -0700

    p2-prepare now has the ability to read p2-may-run and install the required pods, before watching other intents. added health check before install required pods

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,8 +2,12 @@ package constants
 
 import (
 	"github.com/square/p2/pkg/types"
+	"github.com/square/p2/pkg/util/param"
 )
 
 const (
 	PreparerPodID = types.PodID("p2-preparer")
 )
+
+// Maximum allowed time for a single health check, in seconds
+var HEALTHCHECK_TIMEOUT = param.Int64("healthcheck_timeout", 5)
